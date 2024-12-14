80496902f473617f6ee32ab7999bf9c0e15ae3fb
carlushuang <carlus.huang@amd.com>
2024-12-14 23:28:45 +0800

    fp8-pa

--- csrc/include/attention_ck.h
+++ csrc/include/attention_ck.h
@@ -9,12 +9,15 @@ torch::Tensor pa_fwd_naive(torch::Tensor &Q, //   [num_seqs, num_heads, head_siz
                                              // or[num_batch*seqlen, num_kv_heads, head_size]
                            torch::Tensor &block_tables,
                            torch::Tensor &context_lens,
+                           torch::Tensor &k_dequant_scales,
+                           torch::Tensor &v_dequant_scales,
                            const int max_seq_len,
                            const int num_kv_heads,
                            const float scale_s,
                            const float scale_k,
                            const float scale_v,
                            const int block_size,
+                           const int quant_algo,
                            std::optional<torch::Tensor> kv_qscale = std::nullopt
                            // above are input
 );
--- csrc/pybind/attention_ck_pybind.cu
+++ csrc/pybind/attention_ck_pybind.cu
@@ -20,11 +20,14 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
           py::arg("V"),
           py::arg("block_tables"),
           py::arg("context_lens"),
+          py::arg("k_dequant_scales"),
+          py::arg("v_dequant_scales"),
           py::arg("max_seq_len"),
           py::arg("num_kv_heads"),
           py::arg("scale_s"),
           py::arg("scale_k"),
           py::arg("scale_v"),
           py::arg("block_size"),
+          py::arg("quant_algo"),
           py::arg("kv_qscale") = std::nullopt);
 }
