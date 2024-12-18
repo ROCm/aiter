d2ce209161964e20727c8541de753d26e1301408
chenjun <junchen2@amd.com>
2024-12-18 14:56:31 +0000

    add new cache kernels to rocm_ops.cpp

--- csrc/rocm_ops.cpp
+++ csrc/rocm_ops.cpp
@@ -147,4 +147,15 @@ PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
             py::arg("context_lens"),
             py::arg("K_QScale") = std::nullopt,
             py::arg("V_QScale") = std::nullopt);
+      m.def("reshape_and_cache_with_pertoken_quant", &reshape_and_cache_with_pertoken_quant,
+            "reshape_and_cache_with_pertoken_quant(Tensor key, Tensor value,"
+            "                        Tensor! key_cache,"
+            "                        Tensor! value_cache,"
+            "                        Tensor! k_dequant_scales,"
+            "                        Tensor! v_dequant_scales,"
+            "                        Tensor slot_mapping,"
+            "                        str kv_cache_dtype) -> ()");
+      m.def("convert_fp8", &convert_fp8,
+            "convert_fp8(Tensor! dst_cache, Tensor src_cache, float scale, "
+            "str kv_cache_dtype) -> ()");
 }
