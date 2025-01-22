30efc6f3ca2c3fa4a63207e63cf2adeca17cd6e9
chenjun <junchen2@amd.com>
2025-01-22 07:43:26 +0000

    add fmoe asm g1u1 fp8 smoothquant

--- op_tests/test_moe.py
+++ op_tests/test_moe.py
@@ -125,7 +125,7 @@ def permute_weight_a(x: torch.Tensor) -> torch.Tensor:
     return x_
 
 
-@perftest()
+@perftest(num_warmup=1,num_iters=2)
 def torch_moe_test(hidden_states, w1, w2, topk_weight, topk_ids,
                    # following for int8 quant
                    fc1_scale=None,  # [expert, inter_dim, 1]
@@ -184,7 +184,23 @@ def vllm_moe(hidden_states, w1, w2, topk_weight, topk_ids):
                          inplace=False)
 
 # g1u1 not support smoothquant
-def test_fmoe(dtype, token, model_dim, inter_dim, E, topk, quant=False, use_g1u1=False, quant_dtype=torch.int8):
+def test_fmoe(dtype, token, model_dim, inter_dim, E, topk, quant=False, use_g1u1=False, quant_dtype=torch.int8, use_smooth=False):
+    
+    # no quant moe only support g1u0(smooth)
+    if not quant:
+        use_smooth = True
+        use_g1u1 = False
+    
+    # g1u0 now only support int8 smoothquant
+    if not use_g1u1:
+        use_smooth = True
+        quant_dtype = torch.int8
+
+    # g1u1 now only support  fp8(smooth/no smooth), int8(smooth)
+    if use_g1u1 and quant_dtype == torch.int8:
+        use_smooth = False
+    
+    quantstr = (str(quant_dtype).split('.')[-1] + '|smooth' if use_smooth else '') if quant else "None"
     input = torch.randn((token, model_dim), dtype=dtype, device="cuda")
     if use_g1u1:
         w1 = torch.randn((E, inter_dim*2, model_dim), dtype=dtype, device="cuda") / 10
@@ -236,7 +252,7 @@ def test_fmoe(dtype, token, model_dim, inter_dim, E, topk, quant=False, use_g1u1
         sp1 = (E, inter_dim)
         sp2 = (E, model_dim)
 
-        if use_g1u1:
+        if not use_smooth:
             fc1_smooth_scale = None
             fc2_smooth_scale = None
         else:
@@ -248,7 +264,7 @@ def test_fmoe(dtype, token, model_dim, inter_dim, E, topk, quant=False, use_g1u1
         # ref2 implement
         ref2, avg_c = torch_moe_test(input, w1, w2, topk_weights, topk_ids, 
                                      fc1_scale, fc2_scale, 
-                                     fc1_smooth_scale, fc2_smooth_scale if not use_g1u1 else None)
+                                     fc1_smooth_scale, fc2_smooth_scale)
 
         # b implement
         w1b = shuffle_weight(w1)
@@ -268,7 +284,7 @@ def test_fmoe(dtype, token, model_dim, inter_dim, E, topk, quant=False, use_g1u1
                                             fc1_scale, fc2_scale, 
                                             fc1_smooth_scale, fc2_smooth_scale) / (1024*1024*1024*1024.0)
         bw = num_tb * 1e6 / avg_b
-        print(f"[BW] {token=}, {quant=}, {model_dim=}, {inter_dim=}, {E=}, {topk=}, dtype: {dtype}, asm_bandwidth: {bw:.2f}TB/s")
+        print(f"[BW] {token=}, quant={quantstr}, {model_dim=}, {inter_dim=}, {E=}, {topk=}, dtype: {dtype}, asm_bandwidth: {bw:.2f}TB/s")
 
         # if not use_g1u1:
         #     out_b2, avg_b2 = asm_moe_test(input, w1b, w2b, topk_weights, topk_ids,
@@ -280,8 +296,8 @@ def test_fmoe(dtype, token, model_dim, inter_dim, E, topk, quant=False, use_g1u1
         # out_ck, avg_ck = ck_moe_test(input, w1b, w2b, topk_weights, topk_ids,
         #                              fc1_scale, fc2_scale, 
         #                              fc1_smooth_scale, fc2_smooth_scale)
-        
-        msg = f"[perf] {token=}, {quant=}, {model_dim=}, {inter_dim=}, {E=}, {topk=}, dtype: {dtype}, torch_avg: {avg_c:<8.2f} us, asm_avg: {avg_b:.2f} us, uplift: {avg_c/avg_b-1:.1%}"
+
+        msg = f"[perf] {token=}, quant={quantstr}, {model_dim=}, {inter_dim=}, {E=}, {topk=}, dtype: {dtype}, torch_avg: {avg_c:<8.2f} us, asm_avg: {avg_b:.2f} us, uplift: {avg_c/avg_b-1:.1%}"
         checkAllclose(ref2, out_b, rtol=0.01, atol=100, msg=msg)
         # checkAllclose(ref2, avg_ck, rtol=0.01, atol=100)
 
@@ -310,6 +326,11 @@ for dtype in [torch.bfloat16]:
         for dim in [ 8192]:
             for hdim in [1024]:
                 test_fmoe(dtype, m, dim, hdim, 32, 5, quant=True, use_g1u1=True, quant_dtype=torch.float8_e4m3fnuz)
+# for dtype in [torch.bfloat16]:
+#     for m in [128]:
+#         for dim in [4096, 6144,  8192]:
+#             for hdim in [128, 192, 256, 320, 384, 448, 512, 1024, 4096]:
+#                 test_fmoe(dtype, m, dim, hdim, 32, 5, quant=True, use_g1u1=True, quant_dtype=torch.float8_e4m3fnuz, use_smooth=True)
 # for dtype in [torch.bfloat16]:
 #     for m in [128]:
 #         for dim in [6144]:
