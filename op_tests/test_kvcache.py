20bcb3c39d56dff0f179c7b2739c4ec5ad358f44
valarLip <340077269@qq.com>
2025-01-26 22:20:57 +0800

    add more kvcache test

--- op_tests/test_kvcache.py
+++ op_tests/test_kvcache.py
@@ -26,8 +26,8 @@ def run_torch(key, value, k_cache, v_cache, slot_mapping, block_size, x, asm_lay
         k_scale = quantCfg['k_scale']
         v_scale = quantCfg['v_scale']
         key, k_scale_ = aiter.pertoken_quant(key,
-                                            y_scale_dtype=quantCfg['y_scale_dtype'],
-                                            quant_dtype=quantCfg['quant_dtype'])
+                                             y_scale_dtype=quantCfg['y_scale_dtype'],
+                                             quant_dtype=quantCfg['quant_dtype'])
         k_scale_ = k_scale_.permute(0, 1, 3, 2).view(
             num_batch*num_tokens, num_heads).contiguous()
 
@@ -44,8 +44,8 @@ def run_torch(key, value, k_cache, v_cache, slot_mapping, block_size, x, asm_lay
 
     if quantCfg:
         value, v_scale_ = aiter.pertoken_quant(value,
-                                              y_scale_dtype=quantCfg['y_scale_dtype'],
-                                              quant_dtype=quantCfg['quant_dtype'])
+                                               y_scale_dtype=quantCfg['y_scale_dtype'],
+                                               quant_dtype=quantCfg['quant_dtype'])
         v_scale_ = v_scale_.permute(0, 1, 3, 2).view(
             num_batch*num_tokens, num_heads).contiguous()
 
@@ -138,7 +138,7 @@ def test_reshape_and_cache(ctx_lens: int,
         quantCfg['k_scale'] = k_scale.clone()
         quantCfg['v_scale'] = v_scale.clone()
     out_a, us_a = run_aiter(key, value, k_cache_a, v_cache_a,
-                           slot_mapping, block_size, x, asm_layout, quantCfg)
+                            slot_mapping, block_size, x, asm_layout, quantCfg)
 
     print(f'prefill part: ref vs aiter {us_ref:.2f}us vs {us_a:.2f}us')
     names = ['k_cache', 'v_cache', 'k_scale', 'v_scale']
@@ -172,7 +172,7 @@ def test_reshape_and_cache(ctx_lens: int,
         quantCfg['k_scale'] = k_scale.clone()
         quantCfg['v_scale'] = v_scale.clone()
     out_a, us_a = run_aiter(key, value, k_cache_a, v_cache_a,
-                           slot_mapping, block_size, x, asm_layout, quantCfg)
+                            slot_mapping, block_size, x, asm_layout, quantCfg)
 
     print(f'decode part: ref vs aiter {us_ref:.2f}us vs {us_a:.2f}us')
     names = ['k_cache', 'v_cache', 'k_scale', 'v_scale']
@@ -187,11 +187,15 @@ def test_reshape_and_cache(ctx_lens: int,
 
 test_reshape_and_cache(4097, 128, (8, 1), 128, 16,
                        torch.bfloat16, torch.bfloat16)
-print('start quant fp16->fp8')
+print('\nstart quant fp16->fp8')
 test_reshape_and_cache(4097, 128, (8, 1), 128, 16,
                        torch.float16, torch.float8_e4m3fnuz, quantCfg={'y_scale_dtype': torch.float,
-                                                                        'quant_dtype': torch.float8_e4m3fnuz})
-print('start quant bf16->i8')
+                                                                       'quant_dtype': torch.float8_e4m3fnuz})
+print('\nstart quant fp16->i8')
+test_reshape_and_cache(4097, 128, (8, 1), 128, 16,
+                       torch.float16, torch.int8, quantCfg={'y_scale_dtype': torch.float,
+                                                            'quant_dtype': torch.int8})
+print('\nstart quant bf16->i8')
 test_reshape_and_cache(4097, 128, (8, 1), 128, 16,
                        torch.bfloat16, torch.int8, quantCfg={'y_scale_dtype': torch.float,
                                                              'quant_dtype': torch.int8})
