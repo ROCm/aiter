27b6eaa0ae87965e7d531dd9f8cf212e257b4e79
Lucas Santos <Lucas.Santos@amd.com>
2025-02-20 18:24:20 +0000

    Add Fused_Add RMSNorm Triton Kernel

--- aiter/ops/triton/rmsnorm.py
+++ aiter/ops/triton/rmsnorm.py
@@ -7,7 +7,7 @@ import triton.language as tl
 
 
 @triton.jit
-def _rms_kernel(
+def _rms_norm_kernel(
     # Pointers to matrices
     input_ptr,
     output_ptr,
@@ -23,29 +23,24 @@ def _rms_kernel(
     # Epsilon to avoid division by zero
     epsilon,
     # Meta-parameters
-    ZERO_CENTERED_GAMMA: tl.constexpr,
     BLOCK_SIZE: tl.constexpr,
     USE_BLOCKED: tl.constexpr,
     NUM_PRGMS: tl.constexpr,
 ):
     """
     Note: this is Triton jited function and not meant to be called directly. Call rms_norm function
-    below
+    below.
 
     Applies Root Mean Square Layer Normalization over a mini-batch of inputs.
 
     Key parameters:
     - Input: The input tensor to be normalized with shape (n_rows, n_cols).
-    - Output: The output tensor initialized with shape (n_rows, n_cols).
+    - Output: The output tensor with shape (n_rows, n_cols).
     - G: The learnable weights tensor with shape (n_cols, ).
     """
     # Map the program id to the first row of input and output it should compute.
     row_start = tl.program_id(0)
     col_offsets = tl.arange(0, BLOCK_SIZE)
-    # as older version Triton doesn't support tl.assume and BUFF OPS, comment out for now
-    # tl.assume(input_row_stride >= 0)
-    # tl.assume(output_row_stride >= 0)
-    # tl.assume(row_start >= 0)
 
     if USE_BLOCKED:
         # Persistent loop for rows
@@ -55,9 +50,6 @@ def _rms_kernel(
 
             # Accumulate sum of squares
             n_cols_blks = tl.cdiv(n_cols, BLOCK_SIZE) - 1
-            # older version of triton doesn't accept below init
-            # sum_squares: tl.float32 = 0.
-            # however, with type promoting rule in triton, sum_squares should be always fp32 with below init
             sum_squares = 0.0
             for blk_idx in tl.range(0, n_cols_blks, num_stages=2):
                 cols = blk_idx * BLOCK_SIZE + col_offsets
@@ -88,8 +80,6 @@ def _rms_kernel(
                 x = tl.load(input_ptrs).to(tl.float32)
                 g_ptrs = g_ptr + cols
                 g = tl.load(g_ptrs).to(tl.float32)
-                if ZERO_CENTERED_GAMMA:
-                    g += 1
                 rms_norm = x * norm_factor * g
                 output_ptrs = row_output_ptr + cols
                 tl.store(output_ptrs, rms_norm.to(output_ptr.type.element_ty))
@@ -103,8 +93,6 @@ def _rms_kernel(
             )
             g_ptrs = g_ptr + cols
             g = tl.load(g_ptrs, mask=mask, other=0.0).to(tl.float32)
-            if ZERO_CENTERED_GAMMA:
-                g += 1
             rms_norm = x * norm_factor * g
             output_ptrs = row_output_ptr + cols
             tl.store(output_ptrs, rms_norm.to(output_ptr.type.element_ty), mask=mask)
@@ -122,8 +110,153 @@ def _rms_kernel(
             row_norm = tl.sum(row_norm, axis=-1)
             norm_factor = tl.math.rsqrt((row_norm / n_cols) + epsilon)
 
-            if ZERO_CENTERED_GAMMA:
-                g += 1
+            rms_norm = row * norm_factor * g
+
+            output_ptrs = output_ptr + row_idx * output_row_stride + col_offsets
+            output_ptrs = tl.multiple_of(output_ptrs, (16,))
+            tl.store(output_ptrs, rms_norm.to(output_ptr.type.element_ty), mask=mask)
+
+
+@triton.jit
+def _fused_add_rmsnorm_kernel(
+    # Pointers to matrices
+    input_ptr,
+    output_ptr,
+    res_in_ptr,
+    res_out_ptr,
+    g_ptr,
+    # The stride variables represent how much to increase the ptr by when
+    # moving by 1 element in a particular dimension. E.g. `input_row_stride` is
+    # how much to increase `input_ptr` by to get the element one row down.
+    input_row_stride,
+    output_row_stride,
+    # Matrix dimensions
+    n_rows,
+    n_cols,
+    # Epsilon to avoid division by zero
+    epsilon,
+    # Meta-parameters
+    BLOCK_SIZE: tl.constexpr,
+    USE_BLOCKED: tl.constexpr,
+    NUM_PRGMS: tl.constexpr,
+):
+    """
+    Note: this is Triton jited function and not meant to be called directly. Call
+    rmsnorm2d_fwd_with_add function below.
+
+    Performs an addition between two inputs and then applies Root Mean Square Layer Normalization over
+    the addition result.
+
+    Key parameters:
+    - Input: The input tensor to be normalized with shape (n_rows, n_cols).
+    - Output: The output tensor with shape (n_rows, n_cols).
+    - Res_in: The tensor to be added to the Input tensor with shape (n_rows, n_cols).
+    - Res_out: The tensor in which the addition result will be stored with shape (n_rows, n_cols).
+    - G: The learnable weights tensor with shape (n_cols, ).
+    """
+    # Map the program id to the first row of input and output it should compute.
+    row_start = tl.program_id(0)
+    col_offsets = tl.arange(0, BLOCK_SIZE)
+
+    if USE_BLOCKED:
+        # Persistent loop for rows
+        for row_idx in tl.range(row_start, n_rows, NUM_PRGMS, num_stages=1):
+            row_input_ptr = input_ptr + row_idx * input_row_stride
+            row_output_ptr = output_ptr + row_idx * output_row_stride
+            row_res_in_ptr = res_in_ptr + row_idx * input_row_stride
+            row_res_out_ptr = res_out_ptr + row_idx * input_row_stride
+
+            # Accumulate sum of squares
+            n_cols_blks = tl.cdiv(n_cols, BLOCK_SIZE) - 1
+            sum_squares = 0.0
+            for blk_idx in tl.range(0, n_cols_blks, num_stages=2):
+                cols = blk_idx * BLOCK_SIZE + col_offsets
+                input_ptrs = row_input_ptr + cols
+                input_ptrs = tl.multiple_of(input_ptrs, (16,))
+                x = tl.load(input_ptrs).to(tl.float32)
+                res_in_ptrs = row_res_in_ptr + cols
+                res_in_ptrs = tl.multiple_of(res_in_ptrs, (16,))
+                res_in = tl.load(res_in_ptrs).to(tl.float32)
+                x += res_in
+                # Stores residual_out
+                res_out_ptrs = row_res_out_ptr + cols
+                tl.store(res_out_ptrs, x.to(res_out_ptr.type.element_ty))
+
+                sum_squares += tl.sum(x * x, axis=0)
+
+            # Handle remainder
+            cols = n_cols_blks * BLOCK_SIZE + col_offsets
+            mask = cols < n_cols
+            input_ptrs = row_input_ptr + cols
+            input_ptrs = tl.multiple_of(input_ptrs, (16,))
+            x = tl.load(input_ptrs, mask=mask, other=0.0, cache_modifier=".cg").to(
+                tl.float32
+            )
+            res_in_ptrs = row_res_in_ptr + cols
+            res_in_ptrs = tl.multiple_of(res_in_ptrs, (16,))
+            res_in = tl.load(
+                res_in_ptrs, mask=mask, other=0.0, cache_modifier=".cg"
+            ).to(tl.float32)
+            x += res_in
+            # Stores residual_out
+            res_out_ptrs = row_res_out_ptr + cols
+            tl.store(res_out_ptrs, x.to(res_out_ptr.type.element_ty), mask=mask)
+
+            sum_squares += tl.sum(x * x, axis=0)
+
+            # Compute normalization factor
+            mean_square = sum_squares / n_cols
+            norm_factor = tl.rsqrt(mean_square + epsilon)
+
+            # Normalize and write output
+            for blk_idx in tl.range(0, n_cols_blks, num_stages=2):
+                cols = blk_idx * BLOCK_SIZE + col_offsets
+                res_out_ptrs = row_res_out_ptr + cols
+                res_out_ptrs = tl.multiple_of(res_out_ptrs, (16,))
+                x = tl.load(res_out_ptrs).to(tl.float32)
+                g_ptrs = g_ptr + cols
+                g = tl.load(g_ptrs).to(tl.float32)
+                rms_norm = x * norm_factor * g
+                output_ptrs = row_output_ptr + cols
+                tl.store(output_ptrs, rms_norm.to(output_ptr.type.element_ty))
+
+            # Handle remainder
+            cols = n_cols_blks * BLOCK_SIZE + col_offsets
+            mask = cols < n_cols
+            res_out_ptrs = row_res_out_ptr + cols
+            x = tl.load(res_out_ptrs, mask=mask, other=0.0, cache_modifier=".cg").to(
+                tl.float32
+            )
+            g_ptrs = g_ptr + cols
+            g = tl.load(g_ptrs, mask=mask, other=0.0).to(tl.float32)
+            rms_norm = x * norm_factor * g
+            output_ptrs = row_output_ptr + cols
+            tl.store(output_ptrs, rms_norm.to(output_ptr.type.element_ty), mask=mask)
+
+    else:
+        mask = col_offsets < n_cols
+        for row_idx in tl.range(row_start, n_rows, NUM_PRGMS, num_stages=2):
+            input_ptrs = input_ptr + row_idx * input_row_stride + col_offsets
+            input_ptrs = tl.multiple_of(input_ptrs, (16,))
+            row = tl.load(input_ptrs, mask=mask, other=0.0, cache_modifier=".cg").to(
+                tl.float32
+            )
+            res_in_ptrs = res_in_ptr + row_idx * input_row_stride + col_offsets
+            res_in_ptrs = tl.multiple_of(res_in_ptrs, (16,))
+            res_in = tl.load(
+                res_in_ptrs, mask=mask, other=0.0, cache_modifier=".cg"
+            ).to(tl.float32)
+            row += res_in
+            # Stores residual_out
+            res_out_ptrs = res_out_ptr + row_idx * input_row_stride + col_offsets
+            res_out_ptrs = tl.multiple_of(res_out_ptrs, (16,))
+            tl.store(res_out_ptrs, row.to(res_out_ptr.type.element_ty), mask=mask)
+
+            g = tl.load(g_ptr + col_offsets, mask=mask, other=0.0).to(tl.float32)
+            row_norm = row * row
+            row_norm = tl.sum(row_norm, axis=-1)
+            norm_factor = tl.math.rsqrt((row_norm / n_cols) + epsilon)
+
             rms_norm = row * norm_factor * g
 
             output_ptrs = output_ptr + row_idx * output_row_stride + col_offsets
@@ -148,10 +281,9 @@ def rms_norm(x: torch.Tensor, weight: torch.Tensor, epsilon: float = 1e-6):
     blk_size = min(MAX_FUSED_SIZE, triton.next_power_of_2(n_cols))
     USE_BLOCKED = n_cols > blk_size
     NUM_PRGMS = min(n_rows, get_num_sms())
-    ZERO_CENTERED_GAMMA = False
 
     grid = lambda meta: (NUM_PRGMS,)
-    _rms_kernel[grid](
+    _rms_norm_kernel[grid](
         x,
         y,
         weight,
@@ -160,10 +292,45 @@ def rms_norm(x: torch.Tensor, weight: torch.Tensor, epsilon: float = 1e-6):
         n_rows,
         n_cols,
         epsilon,
-        ZERO_CENTERED_GAMMA,
         blk_size,
         USE_BLOCKED,
         NUM_PRGMS,
     )
 
     return y
+
+
+def rmsnorm2d_fwd_with_add(
+    out: torch.Tensor,
+    input: torch.Tensor,
+    residual_in: torch.Tensor,
+    residual_out: torch.Tensor,
+    weight: torch.Tensor,
+    epsilon: float,
+):
+
+    n_rows, n_cols = input.shape
+
+    MAX_FUSED_SIZE = 65536 // input.element_size()
+    blk_size = min(MAX_FUSED_SIZE, triton.next_power_of_2(n_cols))
+    USE_BLOCKED = n_cols > blk_size
+    NUM_PRGMS = min(n_rows, get_num_sms())
+
+    grid = lambda meta: (NUM_PRGMS,)
+    _fused_add_rmsnorm_kernel[grid](
+        input,
+        out,
+        residual_in,
+        residual_out,
+        weight,
+        input.stride(0),
+        out.stride(0),
+        n_rows,
+        n_cols,
+        epsilon,
+        blk_size,
+        USE_BLOCKED,
+        NUM_PRGMS,
+    )
+
+    return out, residual_out
--- op_tests/triton/test_rmsnorm.py
+++ op_tests/triton/test_rmsnorm.py
@@ -3,28 +3,40 @@ import pytest
 import triton
 import torch.nn.functional as F
 from aiter.ops.triton.rmsnorm import rms_norm
+from aiter.ops.triton.rmsnorm import rmsnorm2d_fwd_with_add
 
 
-def torch_rmsnorm(x, g, ZERO_CENTERED_GAMMA, out_dtype=torch.float16, epsilon=1e-6):
+def torch_rmsnorm(x, g, out_dtype=torch.float16, epsilon=1e-6):
     M, N = x.shape
     # cast to float32 as the triton kernel
     x_f32 = x.float()
     g_f32 = g.float()
     rms = torch.sqrt(torch.sum(x_f32 * x_f32, dim=-1) * 1 / N)
     rsigma = 1.0 / rms
-    if ZERO_CENTERED_GAMMA:
-        g_f32 += 1
     rms_norm_f32 = x_f32 * rsigma.unsqueeze(1) * g_f32
     rms_norm = rms_norm_f32.to(out_dtype)
     return rms_norm
 
 
-# def run_torch(x, weight, eps):
-# output = F.rms_norm(
-# input=x, normalized_shape=(x.shape[-1],), weight=weight, eps=eps
-# )
+def run_torch(x, weight, eps, residual=None):
+    if residual is None:
+        residual_out = None
+        output = torch_rmsnorm(x, weight, x.dtype, eps)
+    else:
+        residual_out = x + residual
+        output = torch_rmsnorm(residual_out, weight, residual_out.dtype, eps)
+    return output, residual_out
 
-# return output
+
+def run_triton(x, weight, eps, residual=None):
+    if residual is None:
+        residual_out = None
+        output = rms_norm(x, weight, eps)
+    else:
+        residual_out = torch.empty_like(x)
+        output = torch.empty_like(x)
+        rmsnorm2d_fwd_with_add(output, x, residual, residual_out, weight, eps)
+    return output, residual_out
 
 
 @pytest.mark.parametrize("in_dtype_str", ["fp32", "fp16", "bf16"])
@@ -35,9 +47,13 @@ def torch_rmsnorm(x, g, ZERO_CENTERED_GAMMA, out_dtype=torch.float16, epsilon=1e
         (2, 10),
         (8192, 4096),
         (4096, 8192),
+        (8000, 8000),
         (1, 31744),
         (3, 65536),
+        (1, 131072),
         (873, 1245),
+        (23153, 45),
+        (89999, 234),
     ],
 )
 def test_rmsnorm(M, N, in_dtype_str):
@@ -53,13 +69,11 @@ def test_rmsnorm(M, N, in_dtype_str):
     x = torch.randn(M, N, device="cuda", dtype=in_dtype)
     weight = torch.randn(N, device="cuda", dtype=in_dtype)
 
-    y_triton = rms_norm(x, weight, 1e-5)
-
-    # y_torch = run_torch(x, weight, 1e-5)
-    y_torch = torch_rmsnorm(x, weight, False, out_dtype, 1e-5)
+    y_torch, *_ = run_torch(x, weight, 1e-5)
+    y_triton, *_ = run_triton(x, weight, 1e-5)
 
     if out_dtype in (torch.float16, torch.bfloat16):
-        atol, rtol = 1e-3, 1e-2
+        atol, rtol = 1e-2, 1e-2
     else:
         # float32 typically can be tighter
         atol, rtol = 1e-5, 1e-5
@@ -72,3 +86,54 @@ def test_rmsnorm(M, N, in_dtype_str):
     ), f"y_torch has dtype={y_torch.dtype}, expected {out_dtype}"
 
     triton.testing.assert_close(y_triton, y_torch, atol=atol, rtol=rtol)
+
+
+@pytest.mark.parametrize("in_dtype_str", ["fp32", "fp16", "bf16"])
+@pytest.mark.parametrize(
+    "M, N",
+    [
+        (1, 4),
+        (2, 10),
+        (8192, 4096),
+        (4096, 8192),
+        (8000, 8000),
+        (1, 31744),
+        (3, 65536),
+        (1, 131072),
+        (873, 1245),
+        (23153, 45),
+        (89999, 234),
+    ],
+)
+def test_fused_add_rmsnorm(M, N, in_dtype_str):
+    arg_to_torch_dtype = {
+        "fp16": torch.float16,
+        "bf16": torch.bfloat16,
+        "fp32": torch.float32,
+    }
+    in_dtype = arg_to_torch_dtype[in_dtype_str]
+    out_dtype = in_dtype
+    torch.manual_seed(0)
+
+    x = torch.randn(M, N, device="cuda", dtype=in_dtype)
+    weight = torch.randn(N, device="cuda", dtype=in_dtype)
+    res = torch.randn(M, N, device="cuda", dtype=in_dtype)
+
+    y_torch, res_torch, *_ = run_torch(x, weight, 1e-5, residual=res)
+    y_triton, res_triton, *_ = run_triton(x, weight, 1e-5, residual=res)
+
+    if out_dtype in (torch.float16, torch.bfloat16):
+        atol, rtol = 1e-2, 1e-2
+    else:
+        # float32 typically can be tighter
+        atol, rtol = 1e-5, 1e-5
+
+    assert (
+        y_triton.dtype == out_dtype
+    ), f"y_triton has dtype={y_triton.dtype}, expected {out_dtype}"
+    assert (
+        y_torch.dtype == out_dtype
+    ), f"y_torch has dtype={y_torch.dtype}, expected {out_dtype}"
+
+    triton.testing.assert_close(y_triton, y_torch, atol=atol, rtol=rtol)
+    triton.testing.assert_close(res_triton, res_torch, atol=atol, rtol=rtol)
