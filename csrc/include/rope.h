03617091ab5b21e54ad7590b3b10dac1f0eb6f9f
Jiming Ruan <Jiming.Ruan@amd.com>
2025-02-25 02:38:07 +0000

    Reuse Freqs Front Part. Part.3. Update comments

--- csrc/include/rope.h
+++ csrc/include/rope.h
@@ -8,14 +8,14 @@
 void rope_fwd_impl(
     torch::Tensor&       output,        // [s, b, h, d]
     const torch::Tensor& input,         // [s, b, h, d]
-    const torch::Tensor& freqs,         // [s, 1, 1, d]
+    const torch::Tensor& freqs,         // [s, 1, 1, d // 2] if reuse_freqs_front_part else [s, 1, 1, d]
     const bool           reuse_freqs_front_part
 );
 
 void rope_bwd_impl(
     torch::Tensor&       input_grads,   // [s, b, h, d]
     const torch::Tensor& output_grads,  // [s, b, h, d]
-    const torch::Tensor& freqs,         // [s, 1, 1, d]
+    const torch::Tensor& freqs,         // [s, 1, 1, d // 2] if reuse_freqs_front_part else [s, 1, 1, d]
     const bool           reuse_freqs_front_part
 );
 
@@ -24,7 +24,7 @@ void rope_2c_fwd_impl(
     torch::Tensor&       output_y,      // [s, b, h, d]
     const torch::Tensor& input_x,       // [s, b, h, d]
     const torch::Tensor& input_y,       // [s, b, h, d]
-    const torch::Tensor& freqs,         // [s, 1, 1, d]
+    const torch::Tensor& freqs,         // [s, 1, 1, d // 2] if reuse_freqs_front_part else [s, 1, 1, d]
     const bool           reuse_freqs_front_part
 );
 
@@ -33,23 +33,23 @@ void rope_2c_bwd_impl(
     torch::Tensor&       input_grads_y, // [s, b, h, d]
     const torch::Tensor& output_grads_x,// [s, b, h, d]
     const torch::Tensor& output_grads_y,// [s, b, h, d]
-    const torch::Tensor& freqs,         // [s, 1, 1, d]
+    const torch::Tensor& freqs,         // [s, 1, 1, d // 2] if reuse_freqs_front_part else [s, 1, 1, d]
     const bool           reuse_freqs_front_part
 );
 
 void rope_cached_fwd_impl(
     torch::Tensor&       output,        // [s, b, h, d]
     const torch::Tensor& input,         // [s, b, h, d]
-    const torch::Tensor& cos,           // [s, 1, 1, d]
-    const torch::Tensor& sin,           // [s, 1, 1, d]
+    const torch::Tensor& cos,           // [s, 1, 1, d // 2] if reuse_freqs_front_part else [s, 1, 1, d]
+    const torch::Tensor& sin,           // [s, 1, 1, d // 2] if reuse_freqs_front_part else [s, 1, 1, d]
     const bool           reuse_freqs_front_part
 );
 
 void rope_cached_bwd_impl(
     torch::Tensor&       input_grads,   // [s, b, h, d]
     const torch::Tensor& output_grads,  // [s, b, h, d]
-    const torch::Tensor& cos,           // [s, 1, 1, d]
-    const torch::Tensor& sin,           // [s, 1, 1, d]
+    const torch::Tensor& cos,           // [s, 1, 1, d // 2] if reuse_freqs_front_part else [s, 1, 1, d]
+    const torch::Tensor& sin,           // [s, 1, 1, d // 2] if reuse_freqs_front_part else [s, 1, 1, d]
     const bool           reuse_freqs_front_part
 );
 
@@ -58,8 +58,8 @@ void rope_cached_2c_fwd_impl(
     torch::Tensor&       output_y,      // [s, b, h, d]
     const torch::Tensor& input_x,       // [s, b, h, d]
     const torch::Tensor& input_y,       // [s, b, h, d]
-    const torch::Tensor& cos,           // [s, 1, 1, d]
-    const torch::Tensor& sin,           // [s, 1, 1, d]
+    const torch::Tensor& cos,           // [s, 1, 1, d // 2] if reuse_freqs_front_part else [s, 1, 1, d]
+    const torch::Tensor& sin,           // [s, 1, 1, d // 2] if reuse_freqs_front_part else [s, 1, 1, d]
     const bool           reuse_freqs_front_part
 );
 
@@ -68,8 +68,8 @@ void rope_cached_2c_bwd_impl(
     torch::Tensor&       input_grads_y, // [s, b, h, d]
     const torch::Tensor& output_grads_x,// [s, b, h, d]
     const torch::Tensor& output_grads_y,// [s, b, h, d]
-    const torch::Tensor& cos,           // [s, 1, 1, d]
-    const torch::Tensor& sin,           // [s, 1, 1, d]
+    const torch::Tensor& cos,           // [s, 1, 1, d // 2] if reuse_freqs_front_part else [s, 1, 1, d]
+    const torch::Tensor& sin,           // [s, 1, 1, d // 2] if reuse_freqs_front_part else [s, 1, 1, d]
     const bool           reuse_freqs_front_part
 );
 
@@ -77,7 +77,7 @@ void rope_thd_fwd_impl(
     torch::Tensor&       output,        // [t, h, d]
     const torch::Tensor& input,         // [t, h, d]
     const torch::Tensor& cu_seqlens,    // [b + 1]
-    const torch::Tensor& freqs,         // [max_s, 1, 1, d]
+    const torch::Tensor& freqs,         // [max_s, 1, 1, d // 2] if reuse_freqs_front_part else [max_s, 1, 1, d], where max_s = cu_seqlens[-1]
     const bool           reuse_freqs_front_part
 );
 
@@ -85,17 +85,17 @@ void rope_thd_bwd_impl(
     torch::Tensor&       input_grads,   // [t, h, d]
     const torch::Tensor& output_grads,  // [t, h, d]
     const torch::Tensor& cu_seqlens,    // [b + 1]
-    const torch::Tensor& freqs,         // [max_s, 1, 1, d]
+    const torch::Tensor& freqs,         // [max_s, 1, 1, d // 2] if reuse_freqs_front_part else [max_s, 1, 1, d], where max_s = cu_seqlens[-1]
     const bool           reuse_freqs_front_part
 );
 
 void rope_2d_fwd_impl(
     torch::Tensor&       output,        // [b, s, h, d] where s = H * W
     const torch::Tensor& input,         // [b, s, h, d] where s = H * W
-    const torch::Tensor& cos_h,         // [1, H', 1,  d // 2] where H' >= H
-    const torch::Tensor& sin_h,         // [1, H', 1,  d // 2] where H' >= H
-    const torch::Tensor& cos_w,         // [1, 1,  W', d // 2] where W' >= W
-    const torch::Tensor& sin_w,         // [1, 1,  W', d // 2] where W' >= W
+    const torch::Tensor& cos_h,         // [1, H', 1,  d // 4] if reuse_freqs_front_part else [1, H', 1,  d // 2], where H' >= H
+    const torch::Tensor& sin_h,         // [1, H', 1,  d // 4] if reuse_freqs_front_part else [1, H', 1,  d // 2], where H' >= H
+    const torch::Tensor& cos_w,         // [1, 1,  W', d // 4] if reuse_freqs_front_part else [1, 1,  W', d // 2], where W' >= W
+    const torch::Tensor& sin_w,         // [1, 1,  W', d // 4] if reuse_freqs_front_part else [1, 1,  W', d // 2], where W' >= W
     const int            img_height,    // H
     const int            img_width,     // W
     const bool           reuse_freqs_front_part
@@ -104,10 +104,10 @@ void rope_2d_fwd_impl(
 void rope_2d_bwd_impl(
     torch::Tensor&       input_grads,   // [b, s, h, d] where s = H * W
     const torch::Tensor& output_grads,  // [b, s, h, d] where s = H * W
-    const torch::Tensor& cos_h,         // [1, H', 1,  d // 2] where H' >= H
-    const torch::Tensor& sin_h,         // [1, H', 1,  d // 2] where H' >= H
-    const torch::Tensor& cos_w,         // [1, 1,  W', d // 2] where W' >= W
-    const torch::Tensor& sin_w,         // [1, 1,  W', d // 2] where W' >= W
+    const torch::Tensor& cos_h,         // [1, H', 1,  d // 4] if reuse_freqs_front_part else [1, H', 1,  d // 2], where H' >= H
+    const torch::Tensor& sin_h,         // [1, H', 1,  d // 4] if reuse_freqs_front_part else [1, H', 1,  d // 2], where H' >= H
+    const torch::Tensor& cos_w,         // [1, 1,  W', d // 4] if reuse_freqs_front_part else [1, 1,  W', d // 2], where W' >= W
+    const torch::Tensor& sin_w,         // [1, 1,  W', d // 4] if reuse_freqs_front_part else [1, 1,  W', d // 2], where W' >= W
     const int            img_height,    // H
     const int            img_width,     // W
     const bool           reuse_freqs_front_part
