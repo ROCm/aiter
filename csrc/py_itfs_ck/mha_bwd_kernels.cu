83de5579cb561ee8cf4497d3469ccbca83da3a60
Jim <jim.guo@amd.com>
2025-02-25 16:38:46 +0800

    update

--- csrc/py_itfs_ck/mha_bwd_kernels.cu
+++ csrc/py_itfs_ck/mha_bwd_kernels.cu
@@ -6,37 +6,6 @@
 #include "py_itfs_common.h"
 #include "mha_common.h"
 
-#include "fmha_bwd.hpp"
-#include "mask.hpp"
-
-struct fmha_bwd_traits_all: public fmha_bwd_traits
-{
-    fmha_bwd_traits_all(const mask_info &mask,
-        std::string dtype,
-        int head_size,
-        bool has_dropout,
-        bool enable_alibi,
-        bool deterministic,
-        bool use_ext_asm,
-        bool is_v3_atomic_fp32,
-        int how_v3_bf16_cvt): fmha_bwd_traits{head_size,
-            head_size,
-            dtype,
-            false, // is_group_mode
-            mask.type,
-            enable_alibi ? bias_enum::alibi : bias_enum::no_bias,
-            false,    // has_dbias
-            has_dropout,
-            false, // s_randval
-            deterministic}, 
-            use_ext_asm{use_ext_asm},
-            is_v3_atomic_fp32{is_v3_atomic_fp32},
-            how_v3_bf16_cvt{how_v3_bf16_cvt} {}
-    bool use_ext_asm;
-    bool is_v3_atomic_fp32;
-    int how_v3_bf16_cvt;
-};
-
 float fmha_bwd_router(fmha_bwd_traits_all traits, fmha_bwd_args args, const ck_tile::stream_config& stream_config) {
     float r = fmha_bwd_v3(traits, args, stream_config);
     if (r == -1) {
