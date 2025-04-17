c001ae47c0065065a3127a10e812ed109c91aa3e
Jim, Guo <jim.guo@amd.com>
2025-04-17 18:59:40 +0800

    update

--- aiter/jit/cpp_extension.py
+++ aiter/jit/cpp_extension.py
@@ -22,7 +22,6 @@ from pathlib import Path
 import errno
 
 import torch
-import torch._appdirs
 from torch.utils.file_baton import FileBaton
 from torch.utils._cpp_extension_versioner import ExtensionVersioner
 from torch.utils.hipify import hipify_python
@@ -79,7 +78,7 @@ CUDA_CLANG_VERSIONS: VersionMap = {
     '11.7': (MINIMUM_CLANG_VERSION, (14, 0)),
 }
 
-__all__ = ["get_default_build_root", "check_compiler_ok_for_platform", "get_compiler_abi_compatibility_and_version", "BuildExtension",
+__all__ = ["check_compiler_ok_for_platform", "get_compiler_abi_compatibility_and_version", "BuildExtension",
            "CppExtension", "CUDAExtension", "include_paths", "library_paths", "load", "load_inline", "is_ninja_available",
            "verify_ninja_availability", "remove_extension_h_precompiler_headers", "get_cxx_compiler", "check_compiler_is_gcc"]
 # Taken directly from python stdlib < 3.9
@@ -288,20 +287,6 @@ def _maybe_write(filename, new_content):
 
     with open(filename, 'w') as source_file:
         source_file.write(new_content)
-
-def get_default_build_root() -> str:
-    """
-    Return the path to the root folder under which extensions will built.
-
-    For each extension module built, there will be one folder underneath the
-    folder returned by this function. For example, if ``p`` is the path
-    returned by this function and ``ext`` the name of an extension, the build
-    folder for the extension will be ``p/ext``.
-
-    This directory is **user-specific** so that multiple users on the same
-    machine won't meet permission issues.
-    """
-    return os.path.realpath(torch._appdirs.user_cache_dir(appname='torch_extensions'))
 
 
 def check_compiler_ok_for_platform(compiler: str) -> bool:
@@ -1325,7 +1310,7 @@ def load(name,
         extra_cuda_cflags,
         extra_ldflags,
         extra_include_paths,
-        build_directory or _get_build_directory(name, verbose),
+        build_directory,
         verbose,
         with_cuda,
         is_python_module,
@@ -1514,158 +1499,6 @@ def remove_extension_h_precompiler_headers():
 
     _remove_if_file_exists(head_file_pch)
     _remove_if_file_exists(head_file_signature)
-
-def load_inline(name,
-                cpp_sources,
-                cuda_sources=None,
-                functions=None,
-                extra_cflags=None,
-                extra_cuda_cflags=None,
-                extra_ldflags=None,
-                extra_include_paths=None,
-                build_directory=None,
-                verbose=False,
-                with_cuda=None,
-                is_python_module=True,
-                with_pytorch_error_handling=True,
-                keep_intermediates=True,
-                use_pch=False):
-    r'''
-    Load a PyTorch C++ extension just-in-time (JIT) from string sources.
-
-    This function behaves exactly like :func:`load`, but takes its sources as
-    strings rather than filenames. These strings are stored to files in the
-    build directory, after which the behavior of :func:`load_inline` is
-    identical to :func:`load`.
-
-    See `the
-    tests <https://github.com/pytorch/pytorch/blob/master/test/test_cpp_extensions_jit.py>`_
-    for good examples of using this function.
-
-    Sources may omit two required parts of a typical non-inline C++ extension:
-    the necessary header includes, as well as the (pybind11) binding code. More
-    precisely, strings passed to ``cpp_sources`` are first concatenated into a
-    single ``.cpp`` file. This file is then prepended with ``#include
-    <torch/extension.h>``.
-
-    Furthermore, if the ``functions`` argument is supplied, bindings will be
-    automatically generated for each function specified. ``functions`` can
-    either be a list of function names, or a dictionary mapping from function
-    names to docstrings. If a list is given, the name of each function is used
-    as its docstring.
-
-    The sources in ``cuda_sources`` are concatenated into a separate ``.cu``
-    file and  prepended with ``torch/types.h``, ``cuda.h`` and
-    ``cuda_runtime.h`` includes. The ``.cpp`` and ``.cu`` files are compiled
-    separately, but ultimately linked into a single library. Note that no
-    bindings are generated for functions in ``cuda_sources`` per  se. To bind
-    to a CUDA kernel, you must create a C++ function that calls it, and either
-    declare or define this C++ function in one of the ``cpp_sources`` (and
-    include its name in ``functions``).
-
-    See :func:`load` for a description of arguments omitted below.
-
-    Args:
-        cpp_sources: A string, or list of strings, containing C++ source code.
-        cuda_sources: A string, or list of strings, containing CUDA source code.
-        functions: A list of function names for which to generate function
-            bindings. If a dictionary is given, it should map function names to
-            docstrings (which are otherwise just the function names).
-        with_cuda: Determines whether CUDA headers and libraries are added to
-            the build. If set to ``None`` (default), this value is
-            automatically determined based on whether ``cuda_sources`` is
-            provided. Set it to ``True`` to force CUDA headers
-            and libraries to be included.
-        with_pytorch_error_handling: Determines whether pytorch error and
-            warning macros are handled by pytorch instead of pybind. To do
-            this, each function ``foo`` is called via an intermediary ``_safe_foo``
-            function. This redirection might cause issues in obscure cases
-            of cpp. This flag should be set to ``False`` when this redirect
-            causes issues.
-
-    Example:
-        >>> # xdoctest: +REQUIRES(env:TORCH_DOCTEST_CPP_EXT)
-        >>> from torch.utils.cpp_extension import load_inline
-        >>> source = """
-        at::Tensor sin_add(at::Tensor x, at::Tensor y) {
-          return x.sin() + y.sin();
-        }
-        """
-        >>> module = load_inline(name='inline_extension',
-        ...                      cpp_sources=[source],
-        ...                      functions=['sin_add'])
-
-    .. note::
-        By default, the Ninja backend uses #CPUS + 2 workers to build the
-        extension. This may use up too many resources on some systems. One
-        can control the number of workers by setting the `MAX_JOBS` environment
-        variable to a non-negative number.
-    '''
-    build_directory = build_directory or _get_build_directory(name, verbose)
-
-    if isinstance(cpp_sources, str):
-        cpp_sources = [cpp_sources]
-    cuda_sources = cuda_sources or []
-    if isinstance(cuda_sources, str):
-        cuda_sources = [cuda_sources]
-
-    cpp_sources.insert(0, '#include <torch/extension.h>')
-
-    if use_pch is True:
-        # Using PreCompile Header('torch/extension.h') to reduce compile time.
-        _check_and_build_extension_h_precompiler_headers(extra_cflags, extra_include_paths)
-    else:
-        remove_extension_h_precompiler_headers()
-
-    # If `functions` is supplied, we create the pybind11 bindings for the user.
-    # Here, `functions` is (or becomes, after some processing) a map from
-    # function names to function docstrings.
-    if functions is not None:
-        module_def = []
-        module_def.append('PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {')
-        if isinstance(functions, str):
-            functions = [functions]
-        if isinstance(functions, list):
-            # Make the function docstring the same as the function name.
-            functions = {f: f for f in functions}
-        elif not isinstance(functions, dict):
-            raise ValueError(f"Expected 'functions' to be a list or dict, but was {type(functions)}")
-        for function_name, docstring in functions.items():
-            if with_pytorch_error_handling:
-                module_def.append(f'm.def("{function_name}", torch::wrap_pybind_function({function_name}), "{docstring}");')
-            else:
-                module_def.append(f'm.def("{function_name}", {function_name}, "{docstring}");')
-        module_def.append('}')
-        cpp_sources += module_def
-
-    cpp_source_path = os.path.join(build_directory, 'main.cpp')
-    _maybe_write(cpp_source_path, "\n".join(cpp_sources))
-
-    sources = [cpp_source_path]
-
-    if cuda_sources:
-        cuda_sources.insert(0, '#include <torch/types.h>')
-        cuda_sources.insert(1, '#include <cuda.h>')
-        cuda_sources.insert(2, '#include <cuda_runtime.h>')
-
-        cuda_source_path = os.path.join(build_directory, 'cuda.cu')
-        _maybe_write(cuda_source_path, "\n".join(cuda_sources))
-
-        sources.append(cuda_source_path)
-
-    return _jit_compile(
-        name,
-        sources,
-        extra_cflags,
-        extra_cuda_cflags,
-        extra_ldflags,
-        extra_include_paths,
-        build_directory,
-        verbose,
-        with_cuda,
-        is_python_module,
-        is_standalone=False,
-        keep_intermediates=keep_intermediates)
 
 
 def get_args(func):
@@ -2066,30 +1899,6 @@ def _get_rocm_arch_flags(cflags: Optional[List[str]] = None) -> List[str]:
     flags = [f'--offload-arch={arch}' for arch in archs]
     flags += ['-fno-gpu-rdc']
     return flags
-
-def _get_build_directory(name: str, verbose: bool) -> str:
-    root_extensions_directory = os.environ.get('TORCH_EXTENSIONS_DIR')
-    if root_extensions_directory is None:
-        root_extensions_directory = get_default_build_root()
-        cu_str = ('cpu' if torch.version.cuda is None else
-                  f'cu{torch.version.cuda.replace(".", "")}')  # type: ignore[attr-defined]
-        python_version = f'py{sys.version_info.major}{sys.version_info.minor}'
-        build_folder = f'{python_version}_{cu_str}'
-
-        root_extensions_directory = os.path.join(
-            root_extensions_directory, build_folder)
-
-    if verbose:
-        print(f'Using {root_extensions_directory} as PyTorch extensions root...', file=sys.stderr)
-
-    build_directory = os.path.join(root_extensions_directory, name)
-    if not os.path.exists(build_directory):
-        if verbose:
-            print(f'Creating extension directory {build_directory}...', file=sys.stderr)
-        # This is like mkdir -p, i.e. will also create parent directories.
-        os.makedirs(build_directory, exist_ok=True)
-
-    return build_directory
 
 
 def _get_num_workers(verbose: bool) -> Optional[int]:
