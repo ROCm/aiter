c1516576cc228197d35b91450c6d9a92d985521e
Jim <jimguo12@amd.com>
2025-04-26 00:28:12 +0800

    remove pybind11

--- csrc/pybind/aiter_enum_pybind.cu
+++ csrc/pybind/aiter_enum_pybind.cu
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: MIT
 // Copyright (c) 2024, Advanced Micro Devices, Inc. All rights reserved.
-#include <pybind11/pybind11.h>
 #include "rocm_ops.hpp"
 #include "aiter_enum.h"
 
-PYBIND11_MODULE(module_aiter_enum, m)
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
 {
     AITER_ENUM_PYBIND;
 }
