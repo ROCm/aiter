fix num k heads to num q heads in grid for causal fused

aiter/ops/triton/mha.py
@@ -3443,9 +3443,9 @@ def _flash_attn_fused_backward(
     }
 
     num_k_pids = (max_seqlen_k + BLOCK_N - 1) // BLOCK_N
-    grid_dkdvdq = (batch * num_k_heads * num_k_pids,)
     NUM_SMS = torch.cuda.get_device_properties("cuda").multi_processor_count
     if causal:
+        grid_dkdvdq = (batch * num_q_heads * num_k_pids,)
         _bwd_kernel_dkdvdq_causal[grid_dkdvdq](
             q, k, v, sm_scale, do, dk, dv, dq,
             softmax_lse, delta,

op_tests/triton_tests/test_mha.py
@@ -826,6 +826,6 @@ def test_mha_fused_backward_varlen(
 
 if __name__ == "__main__":
     # Run the tests
-    test_mha_fused_backward_varlen(4, 1024, 1024, 32, 8, 128, 0.0, True, False, torch.float16)
+    test_mha_fused_backward_varlen(4, 1024, 1024, 32, 8, 128, 0.0, True, False, torch.float32)
     torch.cuda.synchronize()
     print("Fused backward test passes!")