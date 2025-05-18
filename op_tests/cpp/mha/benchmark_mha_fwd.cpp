ba9ec1ae2d60928887ae709b80b97a4985ac4df9
Jim, Guo <jim.guo@amd.com>
2025-05-18 18:06:35 +0800

    align benchmark_mha_fwd.cpp with ck fmha_fwd.cpp

--- op_tests/cpp/mha/benchmark_mha_fwd.cpp
+++ op_tests/cpp/mha/benchmark_mha_fwd.cpp
@@ -66,6 +66,7 @@ auto create_args(int argc, char* argv[])
                 "0",
                 "scale factor of S. 0 means equal to 1/sqrt(hdim).\n"
                 "note when squant=1, this value will be modified by range_q/k")
+        .insert("logits_soft_cap", "0", "attention logits soft capping value.")
         .insert("range_q", "16", "per-tensor quantization range of q. used if squant=1.")
         .insert("range_k", "16", "per-tensor quantization range of k. used if squant=1.")
         .insert("range_v", "16", "per-tensor quantization range of v. used if squant=1.")
@@ -407,6 +408,8 @@ bool run(const ck_tile::ArgParser& arg_parser)
     float scale_s = arg_parser.get_float("scale_s");
     if(scale_s == .0f)
         scale_s = 1.0 / ck_tile::sqrt(static_cast<float>(hdim_q)); // TODO: q ? v ?
+
+    const float logits_soft_cap = arg_parser.get_float("logits_soft_cap");
 
     std::string squant_str = arg_parser.get_str("squant");
     bool squant            = [&]() {
@@ -967,6 +970,8 @@ bool run(const ck_tile::ArgParser& arg_parser)
             args.scale_p = scale_p;
             args.scale_o = scale_o;
 
+            args.logits_soft_cap = logits_soft_cap;
+
             args.stride_bias =
                 (bias.type == bias_enum::alibi ? (bias.rank_info == 0 ? 0 : nhead) : stride_bias);
             args.stride_o          = stride_o;
@@ -1255,6 +1260,16 @@ bool run(const ck_tile::ArgParser& arg_parser)
             ck_tile::identity{},
             ck_tile::identity{},
             ck_tile::scales(scale_s));
+
+        if(0.f < logits_soft_cap)
+        {
+            ck_tile::reference_unary_elementwise<SaccDataType, SaccDataType, SaccDataType>(
+                s_host_ref, s_host_ref, [logits_soft_cap](SaccDataType logits) {
+                    return ck_tile::type_convert<SaccDataType>(
+                        logits_soft_cap *
+                        std::tanhf(ck_tile::type_convert<float>(logits / logits_soft_cap)));
+                });
+        }
 
         if(bias.type == bias_enum::elementwise_bias)
         {
