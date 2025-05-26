36ca1714b00c108f869fa0d91687dde23d2cbeb5
fsx950223 <fsx950223@outlook.com>
2025-05-26 09:51:17 +0000

    revert

--- csrc/include/quant.h
+++ csrc/include/quant.h
@@ -4,6 +4,8 @@
 
 #include <torch/torch.h>
 
+namespace aiter {
+
 void static_per_tensor_quant(torch::Tensor &out,          // [..., d]
                              torch::Tensor const &input,  // [..., d]
                              torch::Tensor const &scale); // [1]
@@ -12,7 +14,9 @@ void dynamic_per_tensor_quant(torch::Tensor &out,         // [..., d]
                               torch::Tensor const &input, // [..., d]
                               torch::Tensor &scale);      // [1]
 
-void dynamic_per_token_scaled_quant(torch::Tensor &out,         // [..., d]
-                                    torch::Tensor const &input, // [..., d]
-                                    torch::Tensor &scales,
-                                    std::optional<at::Tensor> const &scale_ub);
+void dynamic_per_token_scaled_quant(
+    torch::Tensor &out,         // [..., d]
+    torch::Tensor const &input, // [..., d]
+    torch::Tensor &scales, std::optional<at::Tensor> const &scale_ub);
+
+} // namespace aiter
