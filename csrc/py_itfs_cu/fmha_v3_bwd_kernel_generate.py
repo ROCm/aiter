e96ed8f7c5caabc819a4e7ae5342d26e4b0f9392
Jim,Guo <jim.guo@amd.com>
2025-05-28 22:00:01 +0800

    update

--- csrc/py_itfs_cu/fmha_v3_bwd_kernel_generate.py
+++ csrc/py_itfs_cu/fmha_v3_bwd_kernel_generate.py
@@ -2726,9 +2726,7 @@ def write_blobs(
     ts_kv = 192 if get_gfx() == "gfx942" else 256
     seqlen_limit = 64 if get_gfx() == "gfx942" else 256
     dqdkdv_kernel = FMHA_BWD_KERNEL_HEADER + FMHA_BWD_API.format(
-        F_AITER_ASM_DIR=get_asm_dir(),
-        F_tile_size_kv=ts_kv,
-        F_seqlen_limit=seqlen_limit
+        F_AITER_ASM_DIR=get_asm_dir(), F_tile_size_kv=ts_kv, F_seqlen_limit=seqlen_limit
     )
     (output_dir / FMHA_BWD_API_FILENAME).write_text(dqdkdv_kernel)
 
