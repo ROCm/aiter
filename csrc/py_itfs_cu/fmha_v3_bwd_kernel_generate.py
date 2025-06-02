fae43f4a2f53fc0a7d04605dc29871202c3e96d7
Jim,Guo <jim.guo@amd.com>
2025-06-02 13:28:17 +0800

    update

--- csrc/py_itfs_cu/fmha_v3_bwd_kernel_generate.py
+++ csrc/py_itfs_cu/fmha_v3_bwd_kernel_generate.py
@@ -2800,14 +2800,14 @@ def write_blobs(
 
     ts_kv = 192 if get_gfx() == "gfx942" else 256
     seqlen_limit = 64 if get_gfx() == "gfx942" else 256
-    dq_shuffle_kernel_define =  "" if get_gfx() == "gfx942" else DQ_SHUFFLE_KERNEL_DEFINE
+    dq_shuffle_kernel_define = "" if get_gfx() == "gfx942" else DQ_SHUFFLE_KERNEL_DEFINE
     dq_shuffle_kernel_call = "" if get_gfx() == "gfx942" else DQ_SHUFFLE_KERNEL_CALL
     dqdkdv_kernel = FMHA_BWD_KERNEL_HEADER + FMHA_BWD_API.format(
         F_AITER_ASM_DIR=get_asm_dir(),
         F_tile_size_kv=ts_kv,
         F_seqlen_limit=seqlen_limit,
         F_dq_shuffle_kernel_define=dq_shuffle_kernel_define,
-        F_dq_shuffle_kernel_call=dq_shuffle_kernel_call
+        F_dq_shuffle_kernel_call=dq_shuffle_kernel_call,
     )
     (output_dir / FMHA_BWD_API_FILENAME).write_text(dqdkdv_kernel)
 
