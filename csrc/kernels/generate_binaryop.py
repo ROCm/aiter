b8eb565ee11e56e482573f8ea6c4ff79b34e3733
root <root@hjbog-srdc-51.amd.com>
2025-07-02 08:58:09 +0000

    [fix]: binaryop prebuild error
    Signed-off-by: root <root@hjbog-srdc-51.amd.com>

--- csrc/kernels/generate_binaryop.py
+++ csrc/kernels/generate_binaryop.py
@@ -427,7 +427,12 @@ if __name__ == "__main__":
     if args.optype == "all":
         optype_str = "add, sub, mul, div"
     if dtype_str == "all":
-        dtype_str = "float32_float32, bfloat16_bfloat16, float16_float16, float64_float64, bool_bool, int32_int32, int64_int64"
+        all_type = ["float32", "bfloat16", "float16", "float64", "bool", "int32", "int64"]
+        tmp_str = ""
+        for input_dtype in all_type:
+          for other_dtype in all_type:
+            tmp_str += (input_dtype + "_" + other_dtype + ",")
+        dtype_str = tmp_str
     op_list = optype_str.split(",")
     op_list = [x.strip() for x in op_list if x.strip()]
     dtype_list = dtype_str.split(",")
