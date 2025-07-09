fix pre-quant GEMM tests based on func. sign. changes

op_tests/triton_tests/test_batched_gemm_afp4wfp4_pre_quant.py
@@ -144,9 +144,7 @@ def test_batched_gemm_afp4_wfp4_pre_quant(B: int, M: int, N: int, K: int, dtype)
     if not (arch_info.is_fp4_avail()):
         pytest.skip("MXFP4 not supported on this architecture")
 
-    x, w, _, w_scales = generate_batched_gemm_afp4wfp4_pre_quant_inputs(
-        B, M, N, K
-    )
+    x, w, _, w_scales = generate_batched_gemm_afp4wfp4_pre_quant_inputs(B, M, N, K)
     out = torch.empty(B, x.shape[1], w.shape[2], device=x.device, dtype=dtype)
 
     torch_out = run_torch(x, w, w_scales, dtype).to(dtype)

op_tests/triton_tests/test_gemm_afp4wfp4_pre_quant_atomic.py
@@ -135,9 +135,7 @@ def test_gemm_afp4_wfp4_pre_quant(M: int, N: int, K: int, dtype, output: bool):
 
     if output:
         out = torch.zeros((M, N), device=x.device, dtype=torch.float32)
-        out = gemm_afp4wfp4_pre_quant(x, w, w_scales, torch.float32, out).to(
-            dtype
-        )
+        out = gemm_afp4wfp4_pre_quant(x, w, w_scales, torch.float32, out).to(dtype)
     else:
         out = gemm_afp4wfp4_pre_quant(x, w, w_scales, torch.float32).to(dtype)
 