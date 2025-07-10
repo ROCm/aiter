c3a42f1bced16c32d5a78da9c609a43b2c805233
Clement Lin <clement.lin@amd.com>
2025-07-10 19:26:24 +0800

    Add use_model_sensitive_rmsnorm option for rmsnorm

--- aiter/ops/rmsnorm.py
+++ aiter/ops/rmsnorm.py
@@ -39,6 +39,7 @@ def rms_norm(
     input: Tensor,
     weight: Tensor,
     epsilon: float,
+    use_model_sensitive_rmsnorm: int,
 ):
     """
     CK version of rmsnorm
@@ -48,7 +49,7 @@ def rms_norm(
 
 @compile_ops("module_rmsnorm")
 def rmsnorm2d_fwd(
-    input: torch.Tensor, weight: torch.Tensor, epsilon: float
+    input: torch.Tensor, weight: torch.Tensor, epsilon: float, use_model_sensitive_rmsnorm: int,
 ) -> torch.Tensor: ...
 
 
@@ -60,6 +61,7 @@ def rmsnorm2d_fwd_with_add(
     residual_out: Tensor,
     weight: Tensor,
     epsilon: float,
+    use_model_sensitive_rmsnorm: int,
 ): ...
 
 
@@ -71,6 +73,7 @@ def rmsnorm2d_fwd_with_smoothquant(
     yscale: Tensor,
     weight: Tensor,
     epsilon: float,
+    use_model_sensitive_rmsnorm: int,
 ): ...
 
 
@@ -84,12 +87,13 @@ def rmsnorm2d_fwd_with_add_smoothquant(
     yscale: Tensor,
     weight: Tensor,
     epsilon: float,
+    use_model_sensitive_rmsnorm: int,
 ): ...
 
 
 @compile_ops("module_rmsnorm")
 def rmsnorm2d_fwd_with_dynamicquant(
-    out: Tensor, input: Tensor, yscale: Tensor, weight: Tensor, epsilon: float
+    out: Tensor, input: Tensor, yscale: Tensor, weight: Tensor, epsilon: float, use_model_sensitive_rmsnorm: int,
 ): ...
 
 
@@ -102,4 +106,5 @@ def rmsnorm2d_fwd_with_add_dynamicquant(
     yscale: Tensor,
     weight: Tensor,
     epsilon: float,
+    use_model_sensitive_rmsnorm: int,
 ): ...
