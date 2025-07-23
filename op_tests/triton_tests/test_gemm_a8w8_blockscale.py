63fde410a89542e8928d5bec284e4c35c975a8a8
ShaoChunLee <Shao-Chun.Lee@amd.com>
2025-07-23 16:14:28 +0000

    fix typo

--- op_tests/triton_tests/test_gemm_a8w8_blockscale.py
+++ op_tests/triton_tests/test_gemm_a8w8_blockscale.py
@@ -4,7 +4,7 @@
 import torch
 import triton
 import pytest
-from aiter.ops.triton.gemm_a8w8_blocscale import gemm_a8w8_blockscale
+from aiter.ops.triton.gemm_a8w8_blockscale import gemm_a8w8_blockscale
 from aiter.ops.triton.utils.arch_info import get_fp8_dtypes
 from aiter.ops.triton.utils.types import str_to_torch_dtype
 import torch.nn.functional as F
