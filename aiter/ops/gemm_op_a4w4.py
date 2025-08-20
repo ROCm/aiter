1b853806c397ea00c1e1b0597c7b1329f5f6fdd5
chenjun <junchen2@amd.com>
2025-08-20 12:23:44 +0000

    fix fp4 gemm precision issue

--- aiter/ops/gemm_op_a4w4.py
+++ aiter/ops/gemm_op_a4w4.py
@@ -79,17 +79,21 @@ def gemm_a4w4(
             f"A4W4 GEMM kernel is not supported on gfx942, but got {gfx_arch}!"
         )
     ck_config = get_GEMM_config(m, n, k)
+    # splitK = None
     splitK = 0
     kernelName = ""
     if ck_config is not None:
-        splitK = ck_config["splitK"]
+        splitK = ck_config.get("splitK", None)
         kernelName = ck_config["kernelName"]
     if (
         m < 256
         or (ck_config is not None and kernelName.find("_ZN") == -1)
         # or bias is None
     ):
+        splitK = 0 if splitK is None else splitK
         return gemm_a4w4_blockscale(A, B, A_scale, B_scale, out, splitK=splitK)
+    if splitK is not None and splitK > 0:
+        out.zero_()
     return gemm_a4w4_asm(
         A,
         B,
@@ -101,7 +105,7 @@ def gemm_a4w4(
         alpha,
         beta,
         bpreshuffle,
-        log2_k_split=0,
+        log2_k_split=splitK,
     )
 
 
