984678ce165283f93ceb812e03d5e24152d60c11
ZhangLirong-amd <Lirong.Zhang@amd.com>
2025-08-27 11:04:54 +0800

    fix all_reduce graph breaks

--- aiter/dist/parallel_state.py
+++ aiter/dist/parallel_state.py
@@ -33,6 +33,7 @@ from unittest.mock import patch
 import torch
 import torch.distributed
 from torch.distributed import Backend, ProcessGroup
+from aiter.jit.utils.torch_guard import torch_compile_guard
 
 import os
 from aiter import logger
@@ -101,8 +102,7 @@ def _register_group(group: "GroupCoordinator") -> None:
 
 
 if supports_custom_op():
-    # @torch.library.custom_op("vllm::inplace_all_reduce",
-    #                          mutates_args=["tensor"])
+    @torch_compile_guard(mutates_args=["tensor"])
     def inplace_all_reduce(tensor: torch.Tensor, group_name: str) -> None:
         assert group_name in _groups, f"Group {group_name} is not found."
         group = _groups[group_name]()
@@ -114,7 +114,7 @@ if supports_custom_op():
     # def _(tensor: torch.Tensor, group_name: str) -> None:
     #     return
 
-    # @torch.library.custom_op("vllm::outplace_all_reduce", mutates_args=[])
+    @torch_compile_guard()
     def outplace_all_reduce(
         tensor: torch.Tensor, open_fp8_quant: bool, group_name: str
     ) -> torch.Tensor:
