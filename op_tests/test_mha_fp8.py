45d8d2e04521ed775d8e0013ef06de82f0bf8bf3
rocking <ChunYu.Lai@amd.com>
2025-08-29 15:07:51 -0500

    Support fp8 fmha

---
+++ op_tests/test_mha_fp8.py
@@ -0,0 +1,208 @@
+# SPDX-License-Identifier: MIT
+# Copyright (C) 2024-2025, Advanced Micro Devices, Inc. All rights reserved.
+
+import torch
+import aiter
+from aiter import dtypes
+from aiter import per_tensor_quant
+from aiter.test_mha_common import (
+    attention_ref,
+    attn_bias_from_alibi_slopes,
+    ck_randval_to_dropout_mask,
+    convert_flash_attn_S_to_softmax,
+)
+import pytest
+import argparse
+
+
+def run_ck(
+    q,
+    k,
+    v,
+    causal=False,
+    window_size=(-1, -1),  # -1 means infinite context window
+):
+    out = aiter.flash_attn_func(
+        q,
+        k,
+        v,
+        dropout_p=0.0,
+        causal=causal,
+        window_size=window_size,
+        bias=None,
+        alibi_slopes=None,
+        deterministic=True,
+        return_lse=False,
+        return_attn_probs=False,
+    )
+
+    return out
+
+
+@pytest.mark.parametrize("mha_type", ["mha", "mqa", "gqa"])
+@pytest.mark.parametrize("local", [False, True])
+@pytest.mark.parametrize("causal", [False, True])
+@pytest.mark.parametrize("batch_size", [5])
+@pytest.mark.parametrize("nheads", [6])
+@pytest.mark.parametrize(
+    "d,d_v",
+    [
+        (32, 32),
+        (40, 40),
+        (59, 59),
+        (64, 64),
+        (96, 96),
+        (111, 111),
+        (128, 128),
+        (160, 160),
+        (192, 192),
+        (224, 224),
+        (256, 256),
+    ],
+)
+@pytest.mark.parametrize(
+    "seqlen_q,seqlen_k",
+    [
+        (113, 203),
+        (128, 217),
+        (113, 211),
+        (108, 256),
+        (256, 512),
+        (512, 256),
+        (1024, 1024),
+        (1023, 1024),
+        (1024, 1023),
+        (2048, 2048),
+    ],
+)
+def test_flash_attn_output(
+    batch_size,
+    nheads,
+    seqlen_q,
+    seqlen_k,
+    d,
+    d_v,
+    causal,
+    local,
+    mha_type,
+):
+    torch.random.manual_seed(0)
+    torch.cuda.empty_cache()
+    nheads_k = nheads if mha_type == "mha" else (1 if mha_type == "mqa" else 3)
+    assert nheads % nheads_k == 0
+    window_size = (-1, -1) if not local else torch.randint(0, seqlen_k, (2,))
+    dtype = torch.bfloat16
+    quant_dtype = dtypes.fp8
+
+    # generate tensor [-1, 1]
+    q = torch.rand(
+        batch_size, seqlen_q, nheads, d, device="cuda", dtype=dtype
+    )
+    k = torch.rand(
+        batch_size, seqlen_k, nheads_k, d, device="cuda", dtype=dtype,
+    )
+    v = torch.rand(
+        batch_size, seqlen_k, nheads_k, d_v, device="cuda", dtype=dtype,
+    )
+
+    # TODO - Support descale
+    q_quant, _ = per_tensor_quant(q, scale=torch.tensor(1), quant_dtype=quant_dtype)
+    k_quant, _ = per_tensor_quant(k, scale=torch.tensor(1), quant_dtype=quant_dtype)
+    v_quant, _ = per_tensor_quant(v, scale=torch.tensor(1), quant_dtype=quant_dtype)
+
+    out = run_ck(q_quant, k_quant, v_quant, causal, window_size)
+    out_ref = run_ck(q, k, v, causal, window_size)
+
+    max_diff = (out - out_ref).abs().max().item()
+    print(f"Output max diff: {max_diff}")
+    assert max_diff < 0.02
+
+
+parser = argparse.ArgumentParser(
+    formatter_class=argparse.RawTextHelpFormatter,
+    description="config input of test",
+)
+parser.add_argument(
+    "-b",
+    "--batch_size",
+    type=int,
+    default=2,
+    help="""Batch size. Default is 2.
+    e.g.: -b 16""",
+)
+parser.add_argument(
+    "-n",
+    "--nheads",
+    type=int,
+    default=5,
+    help="""Number of heads. Default is 5.
+    e.g.: -n 8""",
+)
+parser.add_argument(
+    "-q",
+    "--seqlen_q",
+    type=int,
+    default=512,
+    help="""Sequence length for query. Default is 512.
+    e.g.: -q 1024""",
+)
+parser.add_argument(
+    "-k",
+    "--seqlen_k",
+    type=int,
+    default=512,
+    help="""Sequence length for key. Default is 512.
+    e.g.: -k 1024""",
+)
+parser.add_argument(
+    "-qk",
+    "--d_qk",
+    type=int,
+    default=128,
+    help="""Dimension of query and key. Default is 128.
+    e.g.: -qk 256""",
+)
+parser.add_argument(
+    "-v",
+    "--d_v",
+    type=int,
+    default=128,
+    help="""Dimension of value. Default is 128.
+    e.g.: -v 256""",
+)
+parser.add_argument(
+    "-c",
+    "--causal",
+    action="store_true",
+    help="""Causal attention. Default is False.
+    -c or --causal    # enable causal attention""",
+)
+parser.add_argument(
+    "-l",
+    "--local",
+    action="store_true",
+    help="""Local attention. Default is False.
+    -l or --local    # enable local attention""",
+)
+parser.add_argument(
+    "-m",
+    "--mha_type",
+    type=str,
+    default="mha",
+    help="""Type of multi-head attention.
+    e.g.: -m mha""",
+)
+
+if __name__ == "__main__":
+    args = parser.parse_args()
+    test_flash_attn_output(
+        args.batch_size,
+        args.nheads,
+        args.seqlen_q,
+        args.seqlen_k,
+        args.d_qk,
+        args.d_v,
+        args.causal,
+        args.local,
+        args.mha_type,
+    )
