215c36939092148f9151b3b8864c071f0809c80f
valarLip <340077269@qq.com>
2025-09-22 22:00:23 +0800

    support pybind11_v3

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@
 # Copyright (C) 2024-2025, Advanced Micro Devices, Inc. All rights reserved.
 
 import os
-import sys
 import shutil
+import sys
 
 from setuptools import Distribution, setup
 
@@ -11,12 +11,10 @@ from setuptools import Distribution, setup
 # from aiter.jit import core
 this_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, f"{this_dir}/aiter/")
-from jit import core
-from jit.utils.cpp_extension import (
-    BuildExtension,
-    IS_HIP_EXTENSION,
-)
 from multiprocessing import Pool
+
+from jit import core
+from jit.utils.cpp_extension import IS_HIP_EXTENSION, BuildExtension
 
 ck_dir = os.environ.get("CK_DIR", f"{this_dir}/3rdparty/composable_kernel")
 PACKAGE_NAME = "aiter"
@@ -229,7 +227,7 @@ setup(
     cmdclass={"build_ext": NinjaBuildExtension},
     python_requires=">=3.8",
     install_requires=[
-        "pybind11>=2.13,<3",
+        "pybind11>=2.13",
         # "ninja",
         "pandas",
         "einops",
