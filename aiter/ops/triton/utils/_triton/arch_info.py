fbc715eee32fac3274d1e992b7fe0e133c9544a6
Brayden Zhong <b8zhong@uwaterloo.ca>
2025-10-17 20:21:14 -0700

    remove device check

--- aiter/ops/triton/utils/_triton/arch_info.py
+++ aiter/ops/triton/utils/_triton/arch_info.py
@@ -1,5 +1,5 @@
 import triton
-
+from functools import lru_cache
 # For now, there is 1-to-1 correspondence between arch and device
 _ARCH_TO_DEVICE = {
     "gfx942": "MI300X",
@@ -7,6 +7,7 @@ _ARCH_TO_DEVICE = {
 }
 
 
+@lru_cache(maxsize=1)
 def get_arch():
     try:
         arch = (
@@ -21,6 +22,7 @@ def get_arch():
     return arch
 
 
+@lru_cache(maxsize=1)
 def get_device():
     return _ARCH_TO_DEVICE[get_arch()]
 
