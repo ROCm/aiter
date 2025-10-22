45bd86713b30341a27325ce6993f1cde071eaee2
amd-xiaoyu12 <xiao.yu@amd.com>
2025-10-22 11:34:32 -0400

    Update pa.cuh
    Apply q_scale to fp8 mfma

--- csrc/cpp_itfs/pa/pa.cuh
+++ csrc/cpp_itfs/pa/pa.cuh
@@ -398,12 +398,12 @@ __launch_bounds__(NUM_THREADS) void paged_attention_ll4mi_QKV_mfma16_kernel(
                         q_scale_ptr != nullptr && is_valid
                             ? *(q_scale_ptr + q_scale_idx)
                             : default_scale;
-                    if(q_scale_ptr == nullptr)
+                    if(q_scale_ptr == nullptr) // Add a default per warp q_scale calculation method if q_scale is not provided.
                     {
                        q_max = warpReduceMax(q_max);
                        constexpr float FP8_E4M3_SCALE_TARGET = 224.0f;
                        if constexpr (MFMA_TYPE == MFMAType::Fp8) {
-                          q_scale = q_max > 0 ? FP8_E4M3_SCALE_TARGET / q_max : 1.0f;
+                          q_scale = q_max > 0 ? FP8_E4M3_SCALE_TARGET / q_max : default_scale;
                        }
                     }
                 }
@@ -457,13 +457,13 @@ __launch_bounds__(NUM_THREADS) void paged_attention_ll4mi_QKV_mfma16_kernel(
                                             &Qlocal[gqa_ratio_loop][head_loop][mtp][qkhe_depth][qkratio]
                                                 .xy[i]);
                                         Qtmp8x8.b16x4[i * 2] = __builtin_amdgcn_cvt_pk_fp8_f32(
-                                            to_float<scalar_t>(qptr[0]),
-                                            to_float<scalar_t>(qptr[1]),
+                                            to_float<scalar_t>(qptr[0])*q_scale,
+                                            to_float<scalar_t>(qptr[1])*q_scale,
                                             0,
                                             false);
                                         Qtmp8x8.b16x4[i * 2 + 1] = __builtin_amdgcn_cvt_pk_fp8_f32(
-                                            to_float<scalar_t>(qptr[2]),
-                                            to_float<scalar_t>(qptr[3]),
+                                            to_float<scalar_t>(qptr[2])*q_scale,
+                                            to_float<scalar_t>(qptr[3])*q_scale,
                                             0,
                                             false);
                                     }
