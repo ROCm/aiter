776aec9eb3ba18cf6edd838d0404c43034e3f962
Zhu Jiale <root@hjbog-srdc-52.amd.com>
2025-11-03 11:08:09 +0800

    Merge remote-tracking branch 'origin/feature/rope3d-fix' and resolve conflicts

--- op_tests/test_rope3d.py
+++ op_tests/test_rope3d.py
@@ -3,31 +3,39 @@ import triton
 import triton.language as tl
 from aiter.ops.triton._triton_kernels.rope import _rope_fwd_3d_kernel
 
+
 def rope_params(max_seq_len, dim, theta=10000):
     assert dim % 2 == 0
     freqs = torch.outer(
         torch.arange(max_seq_len),
-        1.0 / torch.pow(theta,
-                        torch.arange(0, dim, 2).to(torch.float32).div(dim))
+        1.0 / torch.pow(theta, torch.arange(0, dim, 2).to(torch.float32).div(dim)),
     )
     freqs = torch.polar(torch.ones_like(freqs), freqs)  # complex
     return freqs
+
 
 def pad_freqs(original_tensor, target_len):
     seq_len, s1, s2 = original_tensor.shape
     pad_size = target_len - seq_len
     padding_tensor = torch.ones(
-        pad_size, s1, s2, dtype=original_tensor.dtype, device=original_tensor.device)
+        pad_size, s1, s2, dtype=original_tensor.dtype, device=original_tensor.device
+    )
     padded_tensor = torch.cat([original_tensor, padding_tensor], dim=0)
     return padded_tensor
 
 
-def rope_apply_triton(x, grid_sizes: tl.constexpr, freqs: tl.constexpr, sp_size: tl.constexpr, sp_rank: tl.constexpr):
+def rope_apply_triton(
+    x,
+    grid_sizes: tl.constexpr,
+    freqs: tl.constexpr,
+    sp_size: tl.constexpr,
+    sp_rank: tl.constexpr,
+):
     B, s, n_heads, C = x.shape
     c_total = C // 2  # 64
     c1 = c_total - 2 * (c_total // 3)  # 22
-    c2 = c_total // 3                  # 21
-    c3 = c_total // 3                  # 21
+    c2 = c_total // 3  # 21
+    c3 = c_total // 3  # 21
     device = x.device
 
     grid_sizes = grid_sizes.to(device=device, dtype=torch.int32).contiguous()
@@ -38,32 +46,43 @@ def rope_apply_triton(x, grid_sizes: tl.constexpr, freqs: tl.constexpr, sp_size:
 
     BLOCK_L, BLOCK_N, BLOCK_C = 32, 4, 64
 
-    grid = (
-        B,
-        n_heads,  
-        triton.cdiv(s, BLOCK_L)
-    )
+    grid = (B, n_heads, triton.cdiv(s, BLOCK_L))
 
     num_warps = 4
     waves_per_eu = 1
 
     _rope_fwd_3d_kernel[grid](
-        x, freqs_real, freqs_imag, grid_sizes, out, 
+        x,
+        freqs_real,
+        freqs_imag,
+        grid_sizes,
+        out,
         *x.stride(),
-        freqs_real.stride(0), freqs_real.stride(1),
+        freqs_real.stride(0),
+        freqs_real.stride(1),
         *grid_sizes.stride(),
         *out.stride(),
-        s, n_heads, C, c_total,
-        sp_size, sp_rank,
-        freqs.shape[0], s,
-        1.0, 0.0,
-        BLOCK_L=BLOCK_L, BLOCK_N=BLOCK_N, BLOCK_C=BLOCK_C,
-        C1=c1, C2=c2,
+        s,
+        n_heads,
+        C,
+        c_total,
+        sp_size,
+        sp_rank,
+        freqs.shape[0],
+        s,
+        1.0,
+        0.0,
+        BLOCK_L=BLOCK_L,
+        BLOCK_N=BLOCK_N,
+        BLOCK_C=BLOCK_C,
+        C1=c1,
+        C2=c2,
         num_warps=num_warps,
         waves_per_eu=waves_per_eu,
     )
 
     return out
+
 
 def rope_apply_original(x, grid_sizes, freqs, sp_size, sp_rank):
     B = x.size(0)
@@ -72,8 +91,8 @@ def rope_apply_original(x, grid_sizes, freqs, sp_size, sp_rank):
     c = x.size(3) // 2
 
     c1 = c - 2 * (c // 3)
-    c2 = (c // 3)
-    c3 = (c // 3)
+    c2 = c // 3
+    c3 = c // 3
     freqs = freqs.split([c1, c2, c3], dim=1)
 
     output = []
@@ -82,15 +101,20 @@ def rope_apply_original(x, grid_sizes, freqs, sp_size, sp_rank):
 
         x_i = torch.view_as_complex(x[i, :s].to(torch.float64).reshape(s, n, -1, 2))
 
-        freqs_i = torch.cat([
-            freqs[0][:f].view(f, 1, 1, -1).expand(f, h, w, -1),
-            freqs[1][:h].view(1, h, 1, -1).expand(f, h, w, -1),
-            freqs[2][:w].view(1, 1, w, -1).expand(f, h, w, -1)
-        ], dim=-1).reshape(seq_len, 1, -1)
+        freqs_i = torch.cat(
+            [
+                freqs[0][:f].view(f, 1, 1, -1).expand(f, h, w, -1),
+                freqs[1][:h].view(1, h, 1, -1).expand(f, h, w, -1),
+                freqs[2][:w].view(1, 1, w, -1).expand(f, h, w, -1),
+            ],
+            dim=-1,
+        ).reshape(seq_len, 1, -1)
         merged_real_sum = freqs_i.real.sum()
         freqs_i = pad_freqs(freqs_i, s * sp_size)
         s_per_rank = s
-        freqs_i_rank = freqs_i[(sp_rank * s_per_rank):((sp_rank + 1) * s_per_rank), :, :]
+        freqs_i_rank = freqs_i[
+            (sp_rank * s_per_rank) : ((sp_rank + 1) * s_per_rank), :, :
+        ]
 
         x_i = torch.view_as_real(x_i * freqs_i_rank).flatten(2)
         x_i = torch.cat([x_i, x[i, s:]])
@@ -99,14 +123,17 @@ def rope_apply_original(x, grid_sizes, freqs, sp_size, sp_rank):
     out = torch.stack(output).float()
     return out
 
+
 def test_rope_consistency():
     B, s, n, C = 1, 9450, 40, 128
     device = "cuda" if torch.cuda.is_available() else "cpu"
     sp_size = 8
     max_seq_len = 1024
 
-    x = torch.arange(B*s*n*C, dtype=torch.float32, device=device).reshape(B, s, n, C)
-    x = x / (B*s*n*C)
+    x = torch.arange(B * s * n * C, dtype=torch.float32, device=device).reshape(
+        B, s, n, C
+    )
+    x = x / (B * s * n * C)
 
     grid_sizes = torch.tensor([[21, 45, 80]], dtype=torch.int32, device=device)
 
@@ -121,20 +148,23 @@ def test_rope_consistency():
     freqs = torch.cat([freqs_f, freqs_h, freqs_w], dim=1).to(device)
 
     sp_rank = 0
-    out_orig = rope_apply_original(x.clone(), grid_sizes.clone(), freqs.clone(), sp_size, sp_rank)
+    out_orig = rope_apply_original(
+        x.clone(), grid_sizes.clone(), freqs.clone(), sp_size, sp_rank
+    )
 
-
-    out_triton = rope_apply_triton(x.clone(), grid_sizes.clone(), freqs.clone(), sp_size, sp_rank)
+    out_triton = rope_apply_triton(
+        x.clone(), grid_sizes.clone(), freqs.clone(), sp_size, sp_rank
+    )
 
     print(f"the result compare: sp_rank={sp_rank}")
-    print("="*50)
-    shape_ok = (out_orig.shape == out_triton.shape)
+    print("=" * 50)
+    shape_ok = out_orig.shape == out_triton.shape
     sum_orig = out_orig.sum().item()
     sum_triton = out_triton.sum().item()
     sum_diff = abs(sum_orig - sum_triton) / abs(sum_orig)
     sum_ok = sum_diff < 1e-2
-    feat_orig = out_orig[0,0,0,:4]
-    feat_triton = out_triton[0,0,0,:4]
+    feat_orig = out_orig[0, 0, 0, :4]
+    feat_triton = out_triton[0, 0, 0, :4]
     feat_diff = torch.abs(feat_orig - feat_triton).max().item()
     feat_ok = feat_diff < 1e-3
 
@@ -148,14 +178,12 @@ def test_rope_consistency():
     print(f"   - Triton:   {feat_triton.cpu().numpy()}")
     print(f"   - max diff: {feat_diff:.6f}")
 
-
     if shape_ok and sum_ok and feat_ok:
         print(f"\n sp_rank={sp_rank} test success")
     else:
         print(f"\n sp_rank={sp_rank} test failed")
-    print("="*60)
+    print("=" * 60)
 
 
 if __name__ == "__main__":
     test_rope_consistency()
-
