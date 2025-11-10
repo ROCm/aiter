b5cd434501c3fd3dd1f3fb8725abb50bb3012642
root <root@hjbog-srdc-24.amd.com>
2025-11-10 07:50:28 +0000

    [fix]: add ar switch
    Signed-off-by: root <root@hjbog-srdc-24.amd.com>

--- csrc/include/custom_all_reduce.cuh
+++ csrc/include/custom_all_reduce.cuh
@@ -1554,11 +1554,11 @@ namespace aiter
       if ((world_size_ <= 4 && bytes < 512 * 1024) ||      \
           (world_size_ <= 8 && bytes < 256 * 1024))        \
       {                                                    \
-        KL(ngpus, cross_device_reduce_1stage_naive);             \
+        KL(ngpus, cross_device_reduce_1stage_naive);       \
       }                                                    \
       else                                                 \
       {                                                    \
-        KL(ngpus, cross_device_reduce_2stage_naive);             \
+        KL(ngpus, cross_device_reduce_2stage_naive);       \
       }                                                    \
     }                                                      \
     break;                                                 \
