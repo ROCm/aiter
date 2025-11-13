04ef66d9a5e0008737aeb7384b1c0b1a3d6b05a4
xytpai <xytpai@foxmail.com>
2025-11-13 10:13:38 +0000

    add fused mrope rms

---
+++ csrc/include/fused_mrope_rms.h
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+// Copyright (c) 2025, Advanced Micro Devices, Inc. All rights reserved.
+
+#pragma once
+
+#include <torch/extension.h>
+
+void fused_mrope_3d_rms(Tensor &qkv, Tensor &qw, Tensor &kw, Tensor &cos_sin, Tensor &positions,
+                    int64_t num_tokens, int64_t num_heads_q, int64_t num_heads_k, int64_t num_heads_v, int64_t head_size,
+                    bool is_neox_style, std::vector<int64_t> mrope_section_, bool is_interleaved, double eps);
