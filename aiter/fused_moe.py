0430e19b6411293e1410b90ac03b23dfcc9da932
antsaukk <Anton.Saukkonen@amd.com>
2025-11-18 19:17:24 +0000

    modidy elif to select run_stage=True for tokens > 16

--- aiter/fused_moe.py
+++ aiter/fused_moe.py
@@ -646,7 +646,7 @@ def get_2stage_cfgs(
             elif q_type == QuantType.per_Token and q_dtype_w == dtypes.i8:
                 run_1stage = token > 32
             elif q_type == QuantType.per_Token and q_dtype_w == dtypes.fp8:
-                run_1stage = True
+                run_1stage = token > 16
             elif q_type != QuantType.per_1x32:
                 run_1stage = token < 256
 
