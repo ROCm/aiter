37400fb83bfd1f921262a4ee7bda18049bdb1bb9
Jiming Ruan <Jiming.Ruan@amd.com>
2025-11-20 08:47:15 +0000

    Framework for HK's MLA

--- csrc/include/rocm_ops.hpp
+++ csrc/include/rocm_ops.hpp
@@ -1673,3 +1673,21 @@ namespace py = pybind11;
           py::arg("rowEnds")   = torch::Tensor(), \
           py::arg("stride0")   = -1,              \
           py::arg("stride1")   = 1);
+
+#define MLA_HK_PYBIND                   \
+    m.def("hk_mla_decode_fwd",          \
+          &hk_mla_decode_fwd,           \
+          "hk_mla_decode_fwd",          \
+          py::arg("query"),             \
+          py::arg("kv_buffer"),         \
+          py::arg("qo_indptr"),         \
+          py::arg("kv_indptr"),         \
+          py::arg("kv_page_indices"),   \
+          py::arg("kv_last_page_lens"), \
+          py::arg("work_indptr"),       \
+          py::arg("work_info_set"),     \
+          py::arg("max_seqlen_q"),      \
+          py::arg("softmax_scale"),     \
+          py::arg("split_data"),        \
+          py::arg("split_lse"),         \
+          py::arg("final_output"));
