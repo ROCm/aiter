25cb1ad1bf9c0c2f2b781bf4bcb9afe94dc2380d
amd-ruitang3 <Rui.Tang2@amd.com>
2025-11-23 16:08:58 +0800

    update

--- hsa/codegen.py
+++ hsa/codegen.py
@@ -115,10 +115,13 @@ using CFG = std::unordered_map<std::string, {args.module}Config>;
 """
                 have_get_header = True
             cfg = [
-                'ADD_CFG('
+                "ADD_CFG("
                 + ", ".join(
-                    (f'{int(getattr(row, col)):>4}' if str(getattr(row, col)).replace('.', '', 1).isdigit()
-                    else f'"{getattr(row, col)}"')
+                    (
+                        f"{int(getattr(row, col)):>4}"
+                        if str(getattr(row, col)).replace(".", "", 1).isdigit()
+                        else f'"{getattr(row, col)}"'
+                    )
                     for col in other_columns
                 )
                 + f', "{row.arch}", "{relpath}/", "{row.knl_name}", "{row.co_name}"),'
