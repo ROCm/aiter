e7ad5efd8d6fdf8bb0847084a3e6690387ca4f4a
Ying.Zhou2 <Ying.Zhou2@amd.com>
2025-11-27 10:07:57 +0800

    fix tuned_gemm and aiter_config error

--- aiter/tuned_gemm.py
+++ aiter/tuned_gemm.py
@@ -184,7 +184,7 @@ def gemm_a16w16(
     m, k = inp_view.shape
     n = B.shape[0]
     use_bias = bias is not None
-    otype = otype if otype is not None else str(inp_view.dtype)
+    otype = otype if otype is not None else inp_view.dtype
     config = get_GEMM_A16W16_config(
         M=m,
         N=n,
