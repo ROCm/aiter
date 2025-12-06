4593775c29d71a746ead15bc603ea82905364893
xiaohuguo2023 <149615094+xiaohuguo2023@users.noreply.github.com>
2025-12-06 12:10:01 +0000

    Update aiter/ops/triton/comms/fused/__init__.py
    Co-authored-by: Muhammad Awad <112003944+mawad-amd@users.noreply.github.com>

--- aiter/ops/triton/comms/fused/__init__.py
+++ aiter/ops/triton/comms/fused/__init__.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: MIT
-# Copyright (C) 2024-2025, Advanced Micro Devices, Inc. All rights reserved.
+# Copyright (C) 2025, Advanced Micro Devices, Inc. All rights reserved.
 
 """
 Fused Communication + Computation Kernels
