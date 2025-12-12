ed98e31d9258aea08b7f8ae9c8cf2bcade07aec5
Satya Nikhil Kodukula <nikhil.kodukula@gmail.com>
2025-12-12 16:41:13 -0600

    test another solution

---
+++ aiter/ops/triton/batched_gemm_a8w8_a_per_token_group_prequant_w_per_batched_tensor_quant.py
@@ -0,0 +1,6 @@
+from aiter.ops.triton.gemm.batched.batched_gemm_a8w8_a_per_token_group_prequant_w_per_batched_tensor_quant import *
+
+__all__ = [
+    "batched_gemm_a8w8_a_per_token_group_prequant_w_per_batched_tensor_quant",
+]
+
