1f3129da2929de30ccf7b5cae88319f00e91a785
ShaoChunLee <Shao-Chun.Lee@amd.com>
2025-12-16 16:15:22 +0000

    fix import

--- op_tests/triton_tests/gemm/fused/test_fused_gemm_a8w8_blockscale_mul_add.py
+++ op_tests/triton_tests/gemm/fused/test_fused_gemm_a8w8_blockscale_mul_add.py
@@ -12,8 +12,8 @@ from op_tests.triton_tests.gemm.basic.test_gemm_a8w8_blockscale import (
 from op_tests.triton_tests.gemm.basic.test_gemm_a8w8_blockscale import (
     run_torch as run_torch_gemm_a8w8_blockscale,
 )
-from op_tests.triton_tests.test_fused_mul_add import generate_fused_mul_add_inputs
-from op_tests.triton_tests.test_fused_mul_add import (
+from op_tests.triton_tests.fusions.test_fused_mul_add import generate_fused_mul_add_inputs
+from op_tests.triton_tests.fusions.test_fused_mul_add import (
     run_torch as run_torch_fused_mul_add,
 )
 
