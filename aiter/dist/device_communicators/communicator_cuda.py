b1d691ee4bd2cd44932c56370f70e32bdb83d8fb
amirumoAMD <Amelia.Moore@amd.com>
2025-12-17 20:49:35 +0000

    test git config

--- aiter/dist/device_communicators/communicator_cuda.py
+++ aiter/dist/device_communicators/communicator_cuda.py
@@ -149,7 +149,7 @@ class CudaCommunicator(DeviceCommunicatorBase):
             qr_comm is not None
             and not qr_comm.disabled
             and qr_comm.should_quick_allreduce(input_)
-            and input_.shape[0] > 512 # input shape should be such that quick reduce will show benefits.
+            #test
             # input shape estimated at 2 * max concurrency for now. if performance issues, subject to change
         ):
             out = qr_comm.quick_all_reduce(input_)
