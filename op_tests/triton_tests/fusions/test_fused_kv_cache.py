57d56f9e75563d22ab0a81afe1d24bdadb0816fe
Satya Nikhil Kodukula <nikhil.kodukula@gmail.com>
2025-12-18 14:20:01 -0600

    fix - black formatting

--- op_tests/triton_tests/fusions/test_fused_kv_cache.py
+++ op_tests/triton_tests/fusions/test_fused_kv_cache.py
@@ -374,7 +374,8 @@ def test_fused_qk_rope_reshape_and_cache(
             triton_key_cache[slot_t, slot_b],
             atol=1e-1,
             rtol=1e-1,
-            equal_nan=arch_info.get_arch() not in ["gfx950"],  # TODO: investigate nan elements for non-gfx950 arch
+            equal_nan=arch_info.get_arch()
+            not in ["gfx950"],  # TODO: investigate nan elements for non-gfx950 arch
         )
         torch.testing.assert_close(
             torch_value_cache[slot_t, slot_b],
