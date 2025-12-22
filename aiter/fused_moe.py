e20d158849e1fda92a36cb0fd91f2838a0d3365a
lalala-sh <Jiaxing.Wen@amd.com>
2025-12-22 09:00:59 +0000

    update

--- aiter/fused_moe.py
+++ aiter/fused_moe.py
@@ -697,7 +697,7 @@ def get_2stage_cfgs(
             elif q_type == QuantType.per_Token and q_dtype_w == dtypes.i8:
                 run_1stage = token > 32
             elif q_type == QuantType.per_Token and q_dtype_w == dtypes.fp8:
-                run_1stage = False
+                run_1stage = token > 16
             elif q_type != QuantType.per_1x32:
                 run_1stage = token < 256
 
