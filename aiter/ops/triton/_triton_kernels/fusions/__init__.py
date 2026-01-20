e38967ce61a25ebc39c3b0805b199dd465f2ecb8
Waqar Ahmed <waqar.ahmed@amd.com>
2026-01-20 06:23:15 -0600

    Add _sinkhorn_knopp_log_domain_kernel to the fusion module

--- aiter/ops/triton/_triton_kernels/fusions/__init__.py
+++ aiter/ops/triton/_triton_kernels/fusions/__init__.py
@@ -1,9 +1,12 @@
 # SPDX-License-Identifier: MIT
 # Copyright (C) 2024-2026, Advanced Micro Devices, Inc. All rights reserved.
 
-from aiter.ops.triton._triton_kernels.fusions.mhc import _mhc_fused_kernel
+from aiter.ops.triton._triton_kernels.fusions.mhc import (
+    _mhc_fused_kernel,
+    _sinkhorn_knopp_log_domain_kernel,
+)
 
 __all__ = [
     "_mhc_fused_kernel",
+    "_sinkhorn_knopp_log_domain_kernel",
 ]
-
