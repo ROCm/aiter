04ec5ee67231e36e1d033e317f80c99c0f7c5f3b
Jim Guo <jim.guo@amd.com>
2026-02-03 01:15:46 -0600

    update kernel

--- op_tests/test_mla_persistent.py
+++ op_tests/test_mla_persistent.py
@@ -531,7 +531,7 @@ def test_mla(
         reduce_indptr,
         reduce_final_map,
         reduce_partial_map,
-        kv_granularity=kv_granularity,  # for qh32 kv split is disabled
+        kv_granularity=max(page_size, 16),  # for qh32 kv split is disabled
         max_seqlen_qo=int(max_seqlen_qo),
         uni_seqlen_qo=decode_qlen,
         fast_mode=True if not non_persistent_mode else False,
