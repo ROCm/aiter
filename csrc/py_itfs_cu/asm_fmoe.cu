55b33147a2c11ccd1802b110e297aea4a9e216cf
Sergey Solo <sergey.solovyev@amd.com>
2026-02-13 23:28:50 +0000

    Improve occupancy for 32x128 fmoe

--- csrc/py_itfs_cu/asm_fmoe.cu
+++ csrc/py_itfs_cu/asm_fmoe.cu
@@ -277,7 +277,7 @@ FMoeKernel* get_heuristic_kernel(
             const auto& cfg = el.second;
             if(cfg.vskip == vskip && cfg.smf == smf)
             {
-                if((inter_dim % cfg.subGU_n) == 0)
+                if((inter_dim % cfg.subGU_n) == 0 && cfg.subGU_n == 128)
                 {
                     tg_num = inter_dim / cfg.subGU_n *
                              sub_X_cnt; // hom many thread_groups are needed to handel inter_dim
@@ -839,7 +839,7 @@ void fmoe_fp8_blockscale_g1u1(torch::Tensor& out,               // [token_cnt, d
     int sub_X_cnt            = sorted_expert_ids.size(0);
     const char* enable_vskip = std::getenv("AITER_ENABLE_VSKIP");
 
-    if(out.dtype() == at::ScalarType::BFloat16 && inter_dim % 256 == 0 && fc_scale_blkn == 128 &&
+    if(out.dtype() == at::ScalarType::BFloat16 && inter_dim % 128 == 0 && fc_scale_blkn == 128 &&
        fc_scale_blkk == 128)
     {
         if(activation == ActivationType::Silu)
