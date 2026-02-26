1074900ff3745b2085f70f62bbe1e99c87316a5f
Juuso Korhonen <40278371+juuso-oskari@users.noreply.github.com>
2026-02-26 09:42:17 +0000

    change default BLOCK_R to 32

--- aiter/ops/triton/attention/fav3_sage_attention_mxfp4_wrapper.py
+++ aiter/ops/triton/attention/fav3_sage_attention_mxfp4_wrapper.py
@@ -70,7 +70,7 @@ class _FAv3SageMXFP4WrapperFunc(torch.autograd.Function):
             hadamard_rotation=hadamard_rotation,
             R=R,
             BLOCK_M=config["BLOCK_M"],
-            BLOCK_R=128 if R is None else R.shape[-1],
+            BLOCK_R=32 if R is None else R.shape[-1],
             q_smoothing=q_smooth,
             layout=layout,
         )
