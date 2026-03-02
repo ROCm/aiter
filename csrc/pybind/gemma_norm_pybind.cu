8bc32b1c37b9c5e7629006595145353c48d4b2ca
apinge <tong.qiu2@amd.com>
2026-03-02 16:58:06 +0800

    update comments
    Signed-off-by: apinge <tong.qiu2@amd.com>

--- csrc/pybind/gemma_norm_pybind.cu
+++ csrc/pybind/gemma_norm_pybind.cu
@@ -1,8 +1,5 @@
 // SPDX-License-Identifier: MIT
-// Copyright (C) 2024-2026, Advanced Micro Devices, Inc. All rights reserved.
-//
-// Gemma norm pybind. Kernel implementation and launch in csrc/kernels/gemma_norm_kernels.cu;
-// API declared in csrc/include/gemma_norm.h (same call chain as rmsnorm).
+// Copyright (C) 2026, Advanced Micro Devices, Inc. All rights reserved.
 #include "rocm_ops.hpp"
 #include "gemma_norm.h"
 
