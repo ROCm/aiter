60f06c32aad91183c31d93071984d0be424fbbfa
Ying.Zhou2 <Ying.Zhou2@amd.com>
2026-03-23 18:05:01 +0800

    recover enum

--- aiter/ops/enum.py
+++ aiter/ops/enum.py
@@ -1,25 +1,16 @@
-# SPDX-License-Identifier: MIT
-# Copyright (C) 2024-2026, Advanced Micro Devices, Inc. All rights reserved.
+from ..jit.core import compile_ops
 
-# Mirror of csrc/include/aiter_enum.h -- update both when changing enum values
-from enum import IntEnum
-
+# from enum import Enum as Enum
 Enum = int
 
 
-class ActivationType(IntEnum):
-    No = -1
-    Silu = 0
-    Gelu = 1
-    Swiglu = 2
+@compile_ops("module_aiter_enum", "ActivationType")
+def _ActivationType(dummy): ...
 
 
-class QuantType(IntEnum):
-    No = 0
-    per_Tensor = 1
-    per_Token = 2
-    per_1x32 = 3
-    per_1x128 = 4
-    per_128x128 = 5
-    per_256x128 = 6
-    per_1024x128 = 7
+@compile_ops("module_aiter_enum", "QuantType")
+def _QuantType(dummy): ...
+
+
+ActivationType = type(_ActivationType(0))
+QuantType = type(_QuantType(0))
