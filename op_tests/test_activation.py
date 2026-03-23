df6a9fb617270f3085d052042ab7c60b995825b6
ChenYou <youchen@amd.com>
2026-03-23 10:44:57 +0800

    fix import error

--- op_tests/test_activation.py
+++ op_tests/test_activation.py
@@ -1,9 +1,11 @@
 import torch
+from torch import Tensor, nn
 import torch.nn.functional as F
-from transformers.activations import ACT2FN
+import math
 import aiter
 from aiter.test_common import run_perftest, checkAllclose, benchmark
 from aiter import dtypes
+import functools
 import pandas as pd
 import argparse
 
@@ -100,8 +102,41 @@ def test_silu_and_mul(m, n, dtype, output_dtype=None):
     return ret
 
 
+class GELUTanh(nn.Module):
+    """
+    A fast C implementation of the tanh approximation of the GeLU activation function. See
+    https://huggingface.co/papers/1606.08415.
+
+    This implementation is equivalent to NewGELU and FastGELU but much faster. However, it is not an exact numerical
+    match due to rounding errors.
+    """
+
+    def __init__(self, use_gelu_tanh_python: bool = False):
+        super().__init__()
+        if use_gelu_tanh_python:
+            self.act = self._gelu_tanh_python
+        else:
+            self.act = functools.partial(nn.functional.gelu, approximate="tanh")
+
+    def _gelu_tanh_python(self, input: Tensor) -> Tensor:
+        return (
+            input
+            * 0.5
+            * (
+                1.0
+                + torch.tanh(
+                    math.sqrt(2.0 / math.pi)
+                    * (input + 0.044715 * torch.pow(input, 3.0))
+                )
+            )
+        )
+
+    def forward(self, input: Tensor) -> Tensor:
+        return self.act(input)
+
+
 def torch_gelu_ref(x: torch.Tensor) -> torch.Tensor:
-    out = ACT2FN["gelu_pytorch_tanh"](x)
+    out = GELUTanh()(x)
     return out
 
 
