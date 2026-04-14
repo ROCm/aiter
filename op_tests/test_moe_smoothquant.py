47d9c195405c6377a1fdabbf3f0277a8d0a7a67c
Li,Tingqian <tingqli.amd.com>
2026-04-14 07:10:43 +0000

    use black to fix file format

--- op_tests/test_moe_smoothquant.py
+++ op_tests/test_moe_smoothquant.py
@@ -5,63 +5,118 @@ from aiter.fused_moe_smoothquant import fused_moe_gelu_sqi8
 from aiter.fused_moe_bf16_asm import torch_moe
 from aiter.test_common import run_perftest
 
+
 def calc_diff(x: torch.Tensor, y: torch.Tensor):
     x, y = x.double(), y.double()
     denominator = (x * x + y * y).sum()
     sim = 2 * (x * y).sum() / denominator
     return 1 - sim
 
-def smooth_quant_w(weight,         # [num_experts, OC, IC]
-                   smooth_scale,   # [num_experts, IC]
-                   ):
+
+def smooth_quant_w(
+    weight,  # [num_experts, OC, IC]
+    smooth_scale,  # [num_experts, IC]
+):
     scaled_weight = weight / smooth_scale
-    per_oc_scale = ((scaled_weight.abs().max(dim=2, keepdim=True)[0])/128) # [num_experts, OC, 1]
-    quanted_weight = (scaled_weight / per_oc_scale).round().clamp(-128, 127).to(torch.int8)
-    return quanted_weight, per_oc_scale # [num_experts, OC, IC], [num_experts, OC, 1]
+    per_oc_scale = (
+        scaled_weight.abs().max(dim=2, keepdim=True)[0]
+    ) / 128  # [num_experts, OC, 1]
+    quanted_weight = (
+        (scaled_weight / per_oc_scale).round().clamp(-128, 127).to(torch.int8)
+    )
+    return quanted_weight, per_oc_scale  # [num_experts, OC, IC], [num_experts, OC, 1]
+
 
 def test_fmoe_sqi8(num_tokens, model_dim, inter_dim, num_experts, topk):
     device = "cuda"
 
     x0 = torch.randn(num_tokens, model_dim, dtype=torch.bfloat16, device=device)
 
-    fc1_smooth_scale = 1.0/torch.randint(low=1, high=5, size=[num_experts, 1, model_dim], dtype=torch.float32, device=device)
-    fc2_smooth_scale = 1.0/torch.randint(low=1, high=5, size=[num_experts, 1, inter_dim], dtype=torch.float32, device=device)
+    fc1_smooth_scale = 1.0 / torch.randint(
+        low=1,
+        high=5,
+        size=[num_experts, 1, model_dim],
+        dtype=torch.float32,
+        device=device,
+    )
+    fc2_smooth_scale = 1.0 / torch.randint(
+        low=1,
+        high=5,
+        size=[num_experts, 1, inter_dim],
+        dtype=torch.float32,
+        device=device,
+    )
 
-    w1_f32 = torch.randn(num_experts, inter_dim, model_dim, dtype=torch.float32).abs()
-    w2_f32 = torch.randn(num_experts, model_dim, inter_dim, dtype=torch.float32).abs()
+    w1_f32 = torch.randn(num_experts, inter_dim, model_dim, dtype=torch.float32)
+    w2_f32 = torch.randn(num_experts, model_dim, inter_dim, dtype=torch.float32)
 
-    w1,fc1_scale = smooth_quant_w(w1_f32, fc1_smooth_scale)
-    w2,fc2_scale = smooth_quant_w(w2_f32, fc2_smooth_scale)
+    w1, fc1_scale = smooth_quant_w(w1_f32, fc1_smooth_scale)
+    w2, fc2_scale = smooth_quant_w(w2_f32, fc2_smooth_scale)
 
     router_weights = torch.randn(num_tokens, num_experts, dtype=torch.float32)
     ret_topk = torch.topk(router_weights, topk)
     x1 = ret_topk.values.to(torch.float32)
     x2 = ret_topk.indices.to(torch.int32)
 
-    ref0 = torch_moe(x0, w1_f32, w2_f32, x1, x2, None, None, None, None, activation=ActivationType.Gelu)
-    ref1 = torch_moe(x0, w1, w2, x1, x2, fc1_scale, fc2_scale, fc1_smooth_scale, fc2_smooth_scale, activation=ActivationType.Gelu)
+    ref0 = torch_moe(
+        x0,
+        w1_f32,
+        w2_f32,
+        x1,
+        x2,
+        None,
+        None,
+        None,
+        None,
+        activation=ActivationType.Gelu,
+    )
+    ref1 = torch_moe(
+        x0,
+        w1,
+        w2,
+        x1,
+        x2,
+        fc1_scale,
+        fc2_scale,
+        fc1_smooth_scale,
+        fc2_smooth_scale,
+        activation=ActivationType.Gelu,
+    )
 
     ret, dt = run_perftest(
-            fused_moe_gelu_sqi8,
-            x0,
-            w1,
-            w2,
-            x1,
-            x2,
-            fc1_scale,
-            fc2_scale,
-            fc1_smooth_scale,
-            fc2_smooth_scale
-        )
+        fused_moe_gelu_sqi8,
+        x0,
+        w1,
+        w2,
+        x1,
+        x2,
+        fc1_scale,
+        fc2_scale,
+        fc1_smooth_scale,
+        fc2_smooth_scale,
+    )
 
-    print(f"{num_tokens=} {model_dim=} {inter_dim=} {num_experts=} {topk=} {calc_diff(ref0, ret)=:.6f}, {dt:.0f} us")
+    print(
+        f"{num_tokens=} {model_dim=} {inter_dim=} {num_experts=} {topk=} {calc_diff(ref0, ret)=:.6f}, {calc_diff(ref1, ret)=:.6f}, {dt:.0f} us"
+    )
+
 
 if __name__ == "__main__":
     torch.set_default_device("cuda")
-    torch.set_printoptions(linewidth=3000, sci_mode=False, edgeitems=8, )
-    #torch.cuda.set_device(0)
+    torch.set_printoptions(
+        linewidth=3000,
+        sci_mode=False,
+        edgeitems=8,
+    )
+    # torch.cuda.set_device(0)
     torch.manual_seed(0)
 
-    test_fmoe_sqi8(num_tokens = 40960, model_dim = 4096, inter_dim = 1536, num_experts = 400, topk = 20)
-    test_fmoe_sqi8(num_tokens = 40960, model_dim = 4096, inter_dim = 1536, num_experts = 800, topk = 25)
-    test_fmoe_sqi8(num_tokens = 19147, model_dim = 4096, inter_dim = 1536, num_experts = 400, topk = 20)
+    test_fmoe_sqi8(
+        num_tokens=40960, model_dim=4096, inter_dim=1536, num_experts=400, topk=20
+    )
+    test_fmoe_sqi8(
+        num_tokens=40960, model_dim=4096, inter_dim=1536, num_experts=800, topk=25
+    )
+    test_fmoe_sqi8(
+        num_tokens=19147, model_dim=4096, inter_dim=1536, num_experts=400, topk=20
+    )
