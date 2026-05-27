f3e9b90d1e1bc4d2b3721c83c9372d9dd71f4737
minmengdie <memin@amd.com>
2026-05-27 02:25:32 +0000

    fix the kernel error

--- op_tests/test_mla_persistent.py
+++ op_tests/test_mla_persistent.py
@@ -459,13 +459,6 @@ def torch_mla_extend_split_kv(
             and nheads == 128
             and is_fp8_q
             and is_fp8_kvc
-            and max_seqlen_q != 4
-        )
-        or (
-            get_gfx() == "gfx950"
-            and nheads == 128
-            and is_fp8_q
-            and is_fp8_kvc
             and is_experimental_enabled()
         )
         or (
@@ -475,13 +468,6 @@ def torch_mla_extend_split_kv(
             and is_fp8_q
             and is_fp8_kvc
             and is_experimental_enabled()
-        )
-        or (
-            get_gfx() == "gfx950"
-            and nheads == 32
-            and is_fp8_q
-            and is_fp8_kvc
-            and max_seqlen_q == 4
         )
         or (
             get_gfx() == "gfx950"
