68aef4cd308df8d895d55fcf32758de1a79a8610
Douglas Lehr <Doug.Lehr@amd.com>
2026-06-16 15:20:07 -0500

    [fused_moe] black: collapse the Swiglu/INTERLEAVE guard onto one line
    Signed-off-by: Douglas Lehr <douglehr@amd.com>

--- aiter/fused_moe.py
+++ aiter/fused_moe.py
@@ -1787,8 +1787,7 @@ def fused_moe_2stages(
         and (
             q_dtype_a in [dtypes.bf16, dtypes.fp16]
             and (
-                activation == ActivationType.Swiglu
-                or gate_mode == GateMode.INTERLEAVE
+                activation == ActivationType.Swiglu or gate_mode == GateMode.INTERLEAVE
             )
             or (q_dtype_a in [dtypes.fp4x2] and metadata.ksplit > 1 and is_shuffled)
         )
