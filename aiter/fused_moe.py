8bbc965e2c72ff13fb331fc009d529f161962103
Felix Li <felix.li@amd.com>
2026-06-18 10:10:13 +0800

    Update fused_moe.py

--- aiter/fused_moe.py
+++ aiter/fused_moe.py
@@ -1786,9 +1786,7 @@ def fused_moe_2stages(
         and w1.dtype == dtypes.fp4x2
         and (
             q_dtype_a in [dtypes.bf16, dtypes.fp16]
-            and (
-                activation == ActivationType.Swiglu or gate_mode == GateMode.INTERLEAVE
-            )
+            and gate_mode == GateMode.INTERLEAVE
             or (q_dtype_a in [dtypes.fp4x2] and metadata.ksplit > 1 and is_shuffled)
         )
     ):
