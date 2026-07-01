ec4463ca321fd0b89bde7fedb398ffea1628d944
Enrico Degregori <enrico@streamhpc.com>
2026-07-01 07:16:40 +0000

    Test shapes

--- csrc/ck_gemm_a8w8/gemm_a8w8_common.py
+++ csrc/ck_gemm_a8w8/gemm_a8w8_common.py
@@ -261,8 +261,8 @@ kernels_list_cktile_95x = {
     #  9:   tileKernelInstance(   128,     128,      128,     1,        4,       1,        16,            16,          128,      "Intrawave",        False,             True,           False,             1      ),
     # 10:   tileKernelInstance(   128,     128,      128,     2,        2,       1,        16,            16,          128,      "Intrawave",        False,             True,           False,             2      ),
     # 8-warp kernel (4x2x1=8)
-    0:   tileKernelInstance(   192,     256,      128,     4,        2,       1,        16,            16,          64,      "Intrawave",        False,             True,           False,             2,     ),
-    # 1:   tileKernelInstance(   256,     256,      128,     4,        2,       1,        16,            16,          64,      "Intrawave",        False,             True,           False,             2,     ),
+    0:   tileKernelInstance(   192,     256,      128,     4,        2,       1,        16,            16,          64,      "Intrawave",        False,            False,           False,             2,     ),
+    1:   tileKernelInstance(   256,     256,      128,     4,        2,       1,        16,            16,          64,      "Intrawave",        False,            False,           False,             2,     ),
     # 0:   tileKernelInstance(   128,     128,      128,     2,        2,       1,        16,            16,          64,      "Intrawave",        False,             True,           False,             2, True      ),
 }
 
