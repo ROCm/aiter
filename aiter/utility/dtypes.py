7dd0b1af9707bc0896e2a5abf393c445e8c429e3
Randall Smith <Randall.Smith@amd.com>
2026-07-01 18:26:35 -0500

    Use the runtime version of get_gfx to avoid issues with GPU_ARCHS
    Signed-off-by: Randall Smith <Randall.Smith@amd.com>

--- aiter/utility/dtypes.py
+++ aiter/utility/dtypes.py
@@ -18,7 +18,7 @@ _8bit_fallback = torch.uint8
 
 
 def get_dtype_fp8():
-    return defaultDtypes.get(get_gfx(), {"fp8": _8bit_fallback})["fp8"]
+    return defaultDtypes.get(get_gfx_runtime(), {"fp8": _8bit_fallback})["fp8"]
 
 
 i4x2 = getattr(torch, "int4", _8bit_fallback)
