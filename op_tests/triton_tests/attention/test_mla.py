7ecc6b5b9c906b47010b21c78b0155923bc630e5
ShaoChunLee <Shao-Chun.Lee@amd.com>
2026-07-29 21:06:01 +0000

    ruff

--- op_tests/triton_tests/attention/test_mla.py
+++ op_tests/triton_tests/attention/test_mla.py
@@ -13,13 +13,10 @@ from aiter.ops.triton.attention.mla import (
 from aiter.ops.triton.utils._triton import arch_info
 from aiter.ops.triton.utils.shuffle import shuffle_scale_batched, shuffle_weight
 from aiter.ops.triton.utils.types import e4m3_dtype
+from aiter.test_common import checkAllclose
 from op_tests.triton_tests.quant.test_quant_mxfp4 import (
     torch_dynamic_mxfp4_quant,
 )
-import aiter.ops.triton.utils._triton.arch_info as arch_info
-from aiter.ops.triton.utils.types import e4m3_dtype
-from aiter.test_common import checkAllclose
-from typing import Optional
 
 DEVICE_ARCH = arch_info.get_arch()
 
