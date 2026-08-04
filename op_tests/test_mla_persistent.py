c91557b48990a7729855185c4973c6617da9e031
minmengdie <memin@amd.com>
2026-08-04 09:23:00 +0000

    fix the err

--- op_tests/test_mla_persistent.py
+++ op_tests/test_mla_persistent.py
@@ -1271,7 +1271,7 @@ def test_mla(
         kv_last_page_lens,
         nhead // nhead_kv,
         nhead_kv,
-        False,
+        causal,
         work_meta_data,
         work_info_set,
         work_indptr,
