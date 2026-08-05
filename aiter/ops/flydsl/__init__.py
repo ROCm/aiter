fd54bc397759e93390beda56995b5adfa509b39b
Waqar Ahmed <waqar.ahmed@amd.com>
2026-08-05 07:15:46 -0500

    Fix ruff I001 and RUF022 in aiter/ops/flydsl/__init__.py

--- aiter/ops/flydsl/__init__.py
+++ aiter/ops/flydsl/__init__.py
@@ -57,9 +57,8 @@ if is_flydsl_available():
         flydsl_pa_mqa_logits_fp4_varqlen,
     )
     from .kernels.qk_norm_rope_quant import flydsl_qk_norm_rope_quant
-    from .moe_kernels import flydsl_moe_stage1, flydsl_moe_stage2
-
     from .linear_attention_kernels import flydsl_gdr_decode
+    from .moe_kernels import flydsl_moe_stage1, flydsl_moe_stage2
 
     __all__ += [
         "FP8_MQA_LOGITS_DEFAULT_VARIANT",
@@ -67,6 +66,7 @@ if is_flydsl_available():
         "compute_varqlen_windows",
         "flydsl_flash_attn_func",
         "flydsl_fp8_mqa_logits",
+        "flydsl_gdr_decode",
         "flydsl_hgemm",
         "flydsl_moe_stage1",
         "flydsl_moe_stage2",
@@ -75,5 +75,4 @@ if is_flydsl_available():
         "flydsl_pa_mqa_logits_fp4_varqlen",
         "flydsl_preshuffle_gemm_a8",
         "flydsl_qk_norm_rope_quant",
-        "flydsl_gdr_decode",
     ]
