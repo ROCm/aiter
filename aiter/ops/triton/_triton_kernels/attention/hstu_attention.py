d20e125ea0698dcbe83cc11c296b77da78ae95f7
Shucai Xiao <shucai.xiao@amd.com>
2026-08-13 11:21:27 -0500

    code cleanup

--- aiter/ops/triton/_triton_kernels/attention/hstu_attention.py
+++ aiter/ops/triton/_triton_kernels/attention/hstu_attention.py
@@ -23,7 +23,6 @@ import triton.language as tl
 
 from aiter.ops.triton.utils._triton import arch_info
 from aiter.ops.triton.utils._triton.kernel_repr import make_kernel_repr
-from aiter.ops.triton.utils._triton.pid_preprocessing import remap_xcd
 from aiter.ops.triton.utils.core import AITER_TRITON_CONFIGS_PATH, load_config_json
 
 try:
