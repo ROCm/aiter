siqiy-cerebras/aiter-review

opus fmha: support symmetric head dim 64 alongside 128
opus_gqa_traits already derives every tile/LDS/MFMA count from D_TILE_SIZE, and
the gqa_d128_kernel body contains no literal head-dim constant -- it is entirely
traits-parameterised. The only thing preventing D=64 was an explicit
static_assert(D_TILE_SIZE_ == 128) plus host-side D==128 checks.

At D=64 every derived quantity stays integral: smem_d_rpt 2->1, GEMM0_E_K 8->4,
GEMM1_E_N 4->2, k/v_buffer_load_insts 2->1, k_ds_read_insts 16->8,
v_ds_read_insts 32->16. Added static_asserts for each of those divisibility
conditions so a future D fails at compile time rather than silently dropping
work.

Changes: relax the traits assert to {64,128}; instantiate Traits64{,Non}Causal
next to the 128 pair and select on D; accept D==64 in launch_d128's checks;
widen the dispatcher to (D_QK==D_V && D_QK in {64,128}); widen the python gate
(_can_impl_fmha_fwd_hd128_bf16_opus -> _can_impl_fmha_fwd_sym_bf16_opus). The
tile shape (Q=32, KV=64, 8 waves) is inherited from D=128 and is NOT retuned
for D=64 -- see below.

Measured on one MI350X (gfx950), b=1 sq=4096 sk=32768 hq=64 hk=8 d=64 bf16,
against the ck_tile path this shape dispatches to today:

  OPUS  d=64     2737.2 us   803.4 TFLOP/s   MFU 34.93%
  ck_tile        3534.8 us   622.1 TFLOP/s   MFU 27.05%
  speedup        1.291x

Correctness vs an fp32 torch reference over four shapes (non-causal, causal,
GQA 64/8, MQA 8/8): max relative error 0.00391. This source tree is covered by
the D=64 cases added to op_tests/test_mha.py alongside the existing D=128
coverage. The D=128 path is preserved by construction -- the tile shape is
inherited unchanged -- and its existing tests still pass.

Co-Authored-By: Claude Opus 5 <noreply@anthropic.com>
Signed-off-by: Siqi Yan <siqi.yan@cerebras.net>

csrc/py_itfs_cu/fmha_fwd_bf16_opus_kernels.cu
@@ -45,11 +45,11 @@ void launch_d128(at::Tensor& q,
     const int H_KV = static_cast<int>(k.size(2));
     const int N_KV = static_cast<int>(k.size(1));      // seqlen_kv (cross-attn: may != N)
 
-    TORCH_CHECK(D == 128, "launch_d128 only compiles D=128, got D=", D);
+    TORCH_CHECK(D == 64 || D == 128, "launch_d_sym compiles D=64 or D=128, got D=", D);
     TORCH_CHECK(k.size(0) == B && v.size(0) == B, "k/v batch must equal q batch B");
     TORCH_CHECK(v.size(1) == N_KV, "k/v seqlen must match (v seqlen != k seqlen)");
     TORCH_CHECK(v.size(2) == H_KV, "k/v must share H_KV");
-    TORCH_CHECK(k.size(3) == D && v.size(3) == D, "k/v head dim must equal D=128");
+    TORCH_CHECK(k.size(3) == D && v.size(3) == D, "k/v head dim must equal q head dim D");
     TORCH_CHECK(H_KV > 0 && (H % H_KV) == 0, "H must be divisible by H_KV (GQA group)");
     TORCH_CHECK(out.size(0) == B && out.size(1) == N && out.size(2) == H && out.size(3) == D,
                 "out shape must match q [B, N, H, D]");
@@ -114,8 +114,12 @@ void launch_d128(at::Tensor& q,
     HipDeviceGuard guard(q.device().index());
     const hipStream_t stream = at::hip::getCurrentHIPStream();
 
-    using TraitsCausal    = opus_gqa_traits<32, 64, 128, 8, true>;
-    using TraitsNonCausal = opus_gqa_traits<32, 64, 128, 8, false>;
+    // Tile shape (Q=32, KV=64, 8 waves) is shared by both head dims; only D_TILE_SIZE
+    // differs, and every derived count in opus_gqa_traits follows from it.
+    using Traits128Causal    = opus_gqa_traits<32, 64, 128, 8, true>;
+    using Traits128NonCausal = opus_gqa_traits<32, 64, 128, 8, false>;
+    using Traits64Causal     = opus_gqa_traits<32, 64, 64, 8, true>;
+    using Traits64NonCausal  = opus_gqa_traits<32, 64, 64, 8, false>;
 
     auto launch = [&](auto traits_tag) {
         using Traits          = decltype(traits_tag);
@@ -127,10 +131,10 @@ void launch_d128(at::Tensor& q,
         HIP_CALL_LAUNCH(hipGetLastError());
     };
 
-    if (causal) {
-        launch(TraitsCausal{});
+    if (D == 128) {
+        if (causal) { launch(Traits128Causal{}); } else { launch(Traits128NonCausal{}); }
     } else {
-        launch(TraitsNonCausal{});
+        if (causal) { launch(Traits64Causal{}); } else { launch(Traits64NonCausal{}); }
     }
 }
 
@@ -369,15 +373,17 @@ void fmha_fwd_bf16_opus_fwd(at::Tensor& q,
     const int D_V  = static_cast<int>(v.size(-1));
     const bool is_group = seqstart_q.has_value() && seqstart_q->numel() > 0;
 
-    if (D_QK == 128 && D_V == 128) {
-        TORCH_CHECK(!is_group, "OPUS D=128 kernel supports batch mode only (no varlen)");
+    if (D_QK == D_V && (D_QK == 64 || D_QK == 128)) {
+        TORCH_CHECK(!is_group, "OPUS symmetric D=", D_QK,
+                    " kernel supports batch mode only (no varlen)");
         launch_d128(q, k, v, out, causal, softmax_scale, lse);
     } else if (D_QK == 192 && D_V == 128) {
         launch_d192_v128(q, k, v, out, causal, softmax_scale, lse,
                          seqstart_q, seqstart_k, seqstart_q_pad, seqstart_k_pad,
                          max_seqlen_q, max_seqlen_k);
     } else {
-        TORCH_CHECK(false, "OPUS fwd supports (D_QK,D_V) in {(128,128),(192,128)}, got (",
+        TORCH_CHECK(false,
+                    "OPUS fwd supports (D_QK,D_V) in {(64,64),(128,128),(192,128)}, got (",
                     D_QK, ",", D_V, ")");
     }
 }

ROCm/aiter

csrc/py_itfs_cu/fmha_fwd_bf16_opus_kernels.cu
@@ -44,11 +44,11 @@ void launch_d128(at::Tensor& q,
     const int H_KV = static_cast<int>(k.size(2));
     const int N_KV = static_cast<int>(k.size(1));      // seqlen_kv (cross-attn: may != N)
 
-    TORCH_CHECK(D == 128, "launch_d128 only compiles D=128, got D=", D);
+    TORCH_CHECK(D == 64 || D == 128, "launch_d128 compiles D=64 or D=128, got D=", D);
     TORCH_CHECK(k.size(0) == B && v.size(0) == B, "k/v batch must equal q batch B");
     TORCH_CHECK(v.size(1) == N_KV, "k/v seqlen must match (v seqlen != k seqlen)");
     TORCH_CHECK(v.size(2) == H_KV, "k/v must share H_KV");
-    TORCH_CHECK(k.size(3) == D && v.size(3) == D, "k/v head dim must equal D=128");
+    TORCH_CHECK(k.size(3) == D && v.size(3) == D, "k/v head dim must equal q head dim D");
     TORCH_CHECK(H_KV > 0 && (H % H_KV) == 0, "H must be divisible by H_KV (GQA group)");
     TORCH_CHECK(out.size(0) == B && out.size(1) == N && out.size(2) == H && out.size(3) == D,
                 "out shape must match q [B, N, H, D]");
@@ -61,7 +61,7 @@ void launch_d128(at::Tensor& q,
     const long long kv_slice_bytes =
         (long long)N_KV * std::max(k.stride(1), v.stride(1)) * 2LL;  // bf16
     TORCH_CHECK(kv_slice_bytes < (1LL << 32),
-                "OPUS D=128: KV byte extent ", kv_slice_bytes,
+                "OPUS D=", D, ": KV byte extent ", kv_slice_bytes,
                 " reaches the 32-bit buffer-offset limit (2^32); reduce seqlen_kv or use another backend");
 
     if (B == 0 || N == 0 || H == 0) return;
@@ -293,15 +293,17 @@ void fmha_fwd_bf16_opus_fwd(at::Tensor& q,
     const int D_V  = static_cast<int>(v.size(-1));
     const bool is_group = seqstart_q.has_value() && seqstart_q->numel() > 0;
 
-    if (D_QK == 128 && D_V == 128) {
-        TORCH_CHECK(!is_group, "OPUS D=128 kernel supports batch mode only (no varlen)");
+    if (D_QK == D_V && (D_QK == 64 || D_QK == 128)) {
+        TORCH_CHECK(!is_group, "OPUS symmetric D=", D_QK,
+                    " kernel supports batch mode only (no varlen)");
         launch_d128(q, k, v, out, causal, softmax_scale, lse);
     } else if (D_QK == 192 && D_V == 128) {
         launch_d192_v128(q, k, v, out, causal, softmax_scale, lse,
                          seqstart_q, seqstart_k, seqstart_q_pad, seqstart_k_pad,
                          max_seqlen_q, max_seqlen_k);
     } else {
-        TORCH_CHECK(false, "OPUS fwd supports (D_QK,D_V) in {(128,128),(192,128)}, got (",
+        TORCH_CHECK(false,
+                    "OPUS fwd supports (D_QK,D_V) in {(64,64),(128,128),(192,128)}, got (",
                     D_QK, ",", D_V, ")");
     }
 }