c8a22955f83a868d5e0deac083e36e432718126e
jiacao-amd <jiahui.cao@amd.com>
2026-09-10 01:07:46 +0000

    perf(kimi-k3): tune merged front decode bucket
    Signed-off-by: jiacao-amd <jiahui.cao@amd.com>

--- op_tests/triton_tests/moe/test_kimi_k3_moe_front.py
+++ op_tests/triton_tests/moe/test_kimi_k3_moe_front.py
@@ -137,7 +137,7 @@ def test_merge_weights_preserves_native_row_order():
     )
 
 
-@pytest.mark.parametrize("m", [7, 14])
+@pytest.mark.parametrize("m", [1, 7, 14, 16])
 def test_decode_full_front_matches_reference(m: int):
     generator = torch.Generator(device="cuda").manual_seed(20260908)
     hidden_states = torch.randn(
