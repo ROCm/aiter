a40a3f5c1b599bb2eaf0102cc52d447cedd4043f
Matti Eskelinen <matti.eskelinen@amd.com>
2026-09-22 07:46:07 +0000

    Fix formatting

--- aiter/ops/flydsl/__init__.py
+++ aiter/ops/flydsl/__init__.py
@@ -50,7 +50,7 @@ _LAZY_IMPORTS = {
     ),
     "flydsl_fused_qk_norm_mrope_3d_cache_pts_quant_shuffle": (
         ".kernels.fused_qk_norm_mrope_3d_cache_pts_quant_shuffle",
-        "flydsl_fused_qk_norm_mrope_3d_cache_pts_quant_shuffle"
+        "flydsl_fused_qk_norm_mrope_3d_cache_pts_quant_shuffle",
     ),
     "flydsl_hgemm": (".gemm_kernels", "flydsl_hgemm"),
     "flydsl_hstu_attention_fwd": (
