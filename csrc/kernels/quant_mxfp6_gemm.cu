38e39403f10e56b624316b738758707283afc8a4
Shantipriya Parida <shantipriya.parida@amd.com>
2026-09-24 09:11:04 -0500

    fix(quant): remove duplicate hip_bfloat16 template specialization in quant_mxfp6_gemm.cu

--- csrc/kernels/quant_mxfp6_gemm.cu
+++ csrc/kernels/quant_mxfp6_gemm.cu
@@ -50,8 +50,6 @@ template <>
 __forceinline__ __device__ float mxfp6_to_float<__half>(__half x) { return __half2float(x); }
 template <>
 __forceinline__ __device__ float mxfp6_to_float<__hip_bfloat16>(__hip_bfloat16 x) { return __bfloat162float(x); }
-template <>
-__forceinline__ __device__ float mxfp6_to_float<hip_bfloat16>(hip_bfloat16 x) { return static_cast<float>(x); }
 
 template <typename input_t>
 __device__ __forceinline__ float to_bf16_dot_operand(input_t input)
