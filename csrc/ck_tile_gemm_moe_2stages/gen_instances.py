43e9cba9de06f6e5131c90a9962bd3dd2681b855
Shantipriya Parida <shantipriya.parida@amd.com>
2026-09-26 11:30:42 +0000

    Apply Black formatting to gen_instances.py has_bias line

--- csrc/ck_tile_gemm_moe_2stages/gen_instances.py
+++ csrc/ck_tile_gemm_moe_2stages/gen_instances.py
@@ -284,7 +284,9 @@ template torch::Tensor
                 "(acc_data_type)": dtype_dict[self.acc_dtype],
                 "(c_data_type)": dtype_dict[self.c_dtype],
                 "(activation)": self.activation,
-                "(has_bias)": "true" if self.activation == 2 and not self.is_split_k else "false",
+                "(has_bias)": (
+                    "true" if self.activation == 2 and not self.is_split_k else "false"
+                ),
                 "(split_k)": "true" if self.is_split_k else "false",
             }
             format_args = {str(key): value.name for key, value in mapping.items()}
